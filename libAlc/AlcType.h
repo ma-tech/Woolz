67c699f6e367b8290f47f6737ee5f6a5475a35f7
bill <bill>
1999-12-01 09:03:03 +0000

    *** empty log message ***

--- libAlc/AlcType.h
+++ libAlc/AlcType.h
@@ -15,6 +15,7 @@
 *		MRC HGU memory allocation library.
 * $Revision$
 * Maintenance:	Log changes below, with most recent at top of list.
+* 01-12-99 bill	Add AlcBlockStack and many comments.
 ************************************************************************/
 
 #ifdef __cplusplus
@@ -30,40 +31,61 @@ typedef enum	/* Error codes returned by MRC HGU type allocation functions */
   ALC_ER_PARAM
 } AlcErrno;
 
+/************************************************************************
+* General purpose data structure for maintaining blocks of some data
+* type. Useful for efficient memory allocation. It's not a stack
+* but a doubly linked list of blocks of data which can be used as a
+* stack, heap, list, ....
+************************************************************************/
+typedef struct _AlcBlockStack
+{
+  int           elmCnt;                  /* Number of elements used in block */
+  int           maxElm;   /* Number of elements space allocated for in block */
+  void          *elements;                              /* Block of elements */
+  struct _AlcBlockStack *prev;                 /* Previous block up in stack */
+  struct _AlcBlockStack *next;   /* Next block down, it's a DLL not a stack! */
+} AlcBlockStack;
+
+/************************************************************************
+* General purpose doubly linked list.
+************************************************************************/
 typedef enum					 /* List traversal direction */
 {
-  ALC_DIRECTION_FWD,
-  ALC_DIRECTION_REV
+  ALC_DIRECTION_FWD,				        /* Towards list tail */
+  ALC_DIRECTION_REV					/* Towards list head */
 } AlcDirection;
 
 typedef struct _AlcDLPItem			  /* Doubly linked list item */
 {
-  void          (*freeFn)(void *);
-  void          *entry;
-  struct _AlcDLPItem *next;
-  struct _AlcDLPItem *prev;
+  void          (*freeFn)(void *);      /* Fn to free list item, may be NULL */
+  void          *entry;				    /* The list item's entry */
+  struct _AlcDLPItem *next;		 /* Next item, towards tail, in list */
+  struct _AlcDLPItem *prev; 	     /* previous item, towards head, in list */
 } AlcDLPItem;
 
 typedef struct _AlcDLPList		   /* Doubly linked list of pointers */
 {
-  AlcDLPItem *head;
+  AlcDLPItem *head;				     /* The head of the list */
 } AlcDLPList;
 
+/************************************************************************
+* General hash table.
+************************************************************************/
 typedef struct _AlcHashItem				  /* Hash table item */
 {
-  void		(*freeFn)(void *);
-  void		*entry;
-  void		*key;
+  void		(*freeFn)(void *);      /* Fn to free hash item, may be NULL */
+  void		*entry;				    /* The hash item's entry */
+  void		*key;		       /* Key which identifies the hash item */
   struct _AlcHashItem *next;		     /* Next item, NULL if last item */
   struct _AlcHashItem *prev; 	        /* Previous item, NULL if first item */
 } AlcHashItem;
 
 typedef struct _AlcHashTable				       /* Hash table */
 {
-  int		(*keyCmp)(void *, void *);
-  unsigned	(*hashFn)(void *);
-  int		tableSz;
-  AlcHashItem	**table;
+  int		(*keyCmp)(void *, void *); 	        /* Key comparison fn */
+  unsigned	(*hashFn)(void *);		           /* The hashing fn */
+  int		tableSz;		    /* Number of list slots in table */
+  AlcHashItem	**table;				   /* Table of lists */
 } AlcHashTable;
 
 #ifdef __cplusplus
