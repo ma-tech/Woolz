11910a6c24b86d3eb5c70802b89c18c5a8d43432
bill <bill>
2001-01-19 16:35:15 +0000

    Add WlzEffHeadReadVtk(), WlzEffReadCtrVtk(), WlzEffReadGMVtk() nad WlzEffReadImgVtk().

--- libWlzExtFF/WlzExtFFVtk.c
+++ libWlzExtFF/WlzExtFFVtk.c
@@ -13,12 +13,15 @@
 *		and from the VTK '.vtk' data format.
 * $Revision$
 * Maintenance:	Log changes below, with most recent at top of list.
+* 10-01-01 bill Add WlzEffHeadReadVtk(), WlzEffReadCtrVtk(),
+*		WlzEffReadGMVtk() nad WlzEffReadImgVtk().
 * 16-08-00 bill	Add WlzEffWriteCtrVtk() and WlzEffWriteGMModelVtk().
 ************************************************************************/
 #include <Wlz.h>
 #include <WlzExtFF.h>
+#include <string.h>
 
-static WlzErrorNum 		WlzEffWriteVoxVtk(
+static WlzErrorNum 		WlzEffWriteImgVtk(
 				  FILE *fP,
 				  WlzObject *obj);
 static WlzErrorNum 		WlzEffWriteCtrVtk(
@@ -27,6 +30,21 @@ static WlzErrorNum 		WlzEffWriteCtrVtk(
 static WlzErrorNum 		WlzEffWriteGMModelVtk(
 				  FILE *fP,
 				  WlzGMModel *model);
+static WlzErrorNum		WlzEffHeadReadVtk(
+				  WlzEffVtkHeader *header,
+				  FILE *fP);
+static WlzObject		*WlzEffReadCtrVtk(
+				  FILE *fP,
+				  WlzEffVtkHeader *header,
+				  WlzErrorNum *dstErr);
+static WlzGMModel		*WlzEffReadGMVtk(
+				  FILE *fP,
+				  WlzEffVtkHeader *header,
+				  WlzErrorNum *dstErr);
+static WlzObject		*WlzEffReadImgVtk(
+				  FILE *fP,
+				  WlzEffVtkHeader *header,
+				  WlzErrorNum *dstErr);
 
 /************************************************************************
 * Function:	WlzEffReadObjVtk
@@ -34,6 +52,8 @@ static WlzErrorNum 		WlzEffWriteGMModelVtk(
 * Purpose:	Reads a Woolz object from the given stream using
 *		the Visualization Toolkit (structured points) file
 *		format.
+*		So far ther is no check on the major and minor version
+*		numbers since all files seem compatable.
 * Global refs:	-
 * Parameters:	FILE *fP:		Input file stream.
 * 		WlzErrorNum *dstErr:	Destination error number ptr,
@@ -42,7 +62,36 @@ static WlzErrorNum 		WlzEffWriteGMModelVtk(
 WlzObject	*WlzEffReadObjVtk(FILE *fP, WlzErrorNum *dstErr)
 {
   WlzObject	*obj = NULL;
-  WlzErrorNum	errNum = WLZ_ERR_READ_INCOMPLETE;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  WlzEffVtkHeader header;
+
+  if(fP == NULL)
+  {
+    errNum = WLZ_ERR_PARAM_NULL;
+  }
+  else
+  {
+    (void )memset((void *)&header, 0, sizeof(WlzEffVtkHeader));
+    errNum = WlzEffHeadReadVtk(&header, fP);
+  }
+  if(errNum == WLZ_ERR_NONE)
+  {
+    switch(header.type)
+    {
+      case WLZEFF_VTK_TYPE_STRUCTURED_POINTS:
+	obj = WlzEffReadImgVtk(fP, &header, &errNum);
+        break;
+      case WLZEFF_VTK_TYPE_POLYDATA:
+	obj = WlzEffReadCtrVtk(fP, &header, &errNum);
+        break;
+      case WLZEFF_VTK_TYPE_STRUCTURED_GRID:   /* FALLTHROUGH */
+      case WLZEFF_VTK_TYPE_UNSTRUCTURED_GRID: /* FALLTHROUGH */
+      case WLZEFF_VTK_TYPE_RECTILNEAR_GRID:   /* FALLTHROUGH */
+      default:
+        errNum = WLZ_ERR_READ_INCOMPLETE;
+        break;
+    }
+  }
   if(dstErr)
   {
     *dstErr = errNum;
@@ -81,7 +130,7 @@ WlzErrorNum	WlzEffWriteObjVtk(FILE *fP, WlzObject *obj)
     switch(obj->type)
     {
       case WLZ_3D_DOMAINOBJ:
-        errNum = WlzEffWriteVoxVtk(fP, obj);
+        errNum = WlzEffWriteImgVtk(fP, obj);
 	break;
       case WLZ_CONTOUR:
         errNum = WlzEffWriteCtrVtk(fP, obj->domain.ctr);
@@ -95,7 +144,7 @@ WlzErrorNum	WlzEffWriteObjVtk(FILE *fP, WlzObject *obj)
 }
 
 /************************************************************************
-* Function:	WlzEffWriteVoxVtk
+* Function:	WlzEffWriteImgVtk
 * Returns:	WlzErrorNum		Woolz error number.
 * Purpose:	Writes the given Woolz 3D domain object with values
 *		object to the given stream using the Visualization
@@ -104,7 +153,7 @@ WlzErrorNum	WlzEffWriteObjVtk(FILE *fP, WlzObject *obj)
 * Parameters:	FILE *fP:		Output file stream.
 *		WlzObject *obj:		Given woolz object.
 ************************************************************************/
-static WlzErrorNum WlzEffWriteVoxVtk(FILE *fP, WlzObject *obj)
+static WlzErrorNum WlzEffWriteImgVtk(FILE *fP, WlzObject *obj)
 {
   unsigned char	***data = NULL;
   WlzErrorNum	errNum = WLZ_ERR_NONE;
@@ -297,7 +346,7 @@ static WlzErrorNum WlzEffWriteGMModelVtk(FILE *fP, WlzGMModel *model)
     {
       case WLZ_GMMOD_2I:
       case WLZ_GMMOD_2D:
-	/* TODO */
+	/* TODO Not sure how to output a 2D model for VTK. */
         break;
       case WLZ_GMMOD_3I:
       case WLZ_GMMOD_3D:
@@ -332,4 +381,391 @@ static WlzErrorNum WlzEffWriteGMModelVtk(FILE *fP, WlzGMModel *model)
     WlzGMModelResIdxFree(resIdxTb);
   }
   return(errNum);
+}
+
+/************************************************************************
+* Function:	WlzEffHeadReadVtk
+* Returns:	WlzErrorNum		Woolz error number.
+* Purpose:	Reads the VTK file header which is
+*		  ident\n
+*		  title\n
+*		  data type\n
+*		  type\n
+*		where
+*		  ident = "vtk DataFile Version MAJOR.MINOR"
+*		  title = less than 256 chars followed by a \n
+*		  data type = "ASCII"|"BINARY"
+*		  type = DATASET "STRUCTURED_POINTS"|"STRUCTURED_GRID"|
+*		         "UNSTRUCTURED_GRID"|"POLYDATA"|
+*		         "RECTILNEAR_GRID"
+* Global refs:	-
+* Parameters:	WlzEffVtkHeader *header: Given header data structure
+*					be filled.
+*		FILE *fP:		Output file stream.
+************************************************************************/
+static WlzErrorNum WlzEffHeadReadVtk(WlzEffVtkHeader *header, FILE *fP)
+{
+  int		valI;
+  char		*valS;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  char		buf[256];
+
+  /* Read and check the identifier, major and minor version numbers are
+   * assumed to be ok. */
+  if(fgets(buf, 256, fP) == NULL)
+  {
+    errNum = WLZ_ERR_READ_INCOMPLETE;
+  }
+  else
+  {
+    buf[255] = '\0';
+    if(sscanf(buf, "# vtk DataFile Version %d.%d",
+	      &(header->versionMajor), &(header->versionMinor)) != 2)
+    {
+      errNum = WLZ_ERR_READ_INCOMPLETE;
+    }
+  }
+  /* Read the title. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    if(fgets(header->title, 256, fP) == NULL)
+    {
+      errNum = WLZ_ERR_READ_INCOMPLETE;
+    }
+    else
+    {
+      buf[255] = '\0';
+    }
+  }
+  /* Read and parse the data type. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    if(fgets(buf, 256, fP) == NULL)
+    {
+      errNum = WLZ_ERR_READ_INCOMPLETE;
+    }
+    else
+    {
+      buf[255] = '\0';
+      valS = strtok(buf, " \t\n");
+      if((valS == NULL) ||
+         (WlzStringMatchValue(&valI, valS,
+      			      "ASCII", WLZEFF_VTK_DATATYPE_ASCII,
+			      "BINARY", WLZEFF_VTK_DATATYPE_BINARY,
+			      NULL) == 0))
+      {
+        errNum = WLZ_ERR_READ_INCOMPLETE;
+      }
+      else
+      {
+        header->dataType = valI;
+      }
+    }
+  }
+  /* Read and parse the type. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    if(fgets(buf, 256, fP) == NULL)
+    {
+      errNum = WLZ_ERR_READ_INCOMPLETE;
+    }
+    else
+    {
+      buf[255] = '\0';
+      valS = strtok(buf, " \t\n");
+      if((valS == NULL) || strcmp(valS, "DATASET"))
+      {
+        errNum = WLZ_ERR_READ_INCOMPLETE;
+      }
+      else
+      {
+        valS = strtok(NULL, " \t\n");
+	if((valS == NULL) ||
+	   (WlzStringMatchValue(&valI, valS,
+	    	"STRUCTURED_POINTS", WLZEFF_VTK_TYPE_STRUCTURED_POINTS,
+	    	"STRUCTURED_GRID", WLZEFF_VTK_TYPE_STRUCTURED_GRID,
+	    	"UNSTRUCTURED_GRID", WLZEFF_VTK_TYPE_UNSTRUCTURED_GRID,
+	    	"POLYDATA", WLZEFF_VTK_TYPE_POLYDATA,
+	    	"RECTILNEAR_GRID", WLZEFF_VTK_TYPE_RECTILNEAR_GRID,
+		NULL) == 0))
+	{
+	  errNum = WLZ_ERR_READ_INCOMPLETE;
+	}
+	else
+	{
+          header->type = valI;
+	}
+      }
+    }
+  }
+  return(errNum);
+}
+
+/************************************************************************
+* Function:	WlzEffReadCtrVtk
+* Returns:	WlzObject *:		Object read from file.
+* Purpose:	Reads a Woolz contour object from the given stream
+*		using the Visualization Toolkit (polydata) file
+*		format.
+* Global refs:	-
+* Parameters:	FILE *fP:		Input file stream.
+*		WlzEffVtkHeader *header: Header data structure.
+* 		WlzErrorNum *dstErr:	Destination error number ptr,
+*					may be NULL.
+************************************************************************/
+WlzObject	*WlzEffReadCtrVtk(FILE *fP, WlzEffVtkHeader *header,
+				  WlzErrorNum *dstErr)
+{
+  WlzDomain	dom;
+  WlzValues	val;
+  WlzObject	*obj = NULL;
+  WlzContour	*ctr = NULL;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+
+  if((ctr = WlzMakeContour(&errNum)) != NULL)
+  {
+    ctr->model = WlzAssignGMModel(WlzEffReadGMVtk(fP, header, &errNum), NULL);
+    if(errNum == WLZ_ERR_NONE)
+    {
+      dom.ctr = ctr;
+      val.core = NULL;
+      obj = WlzMakeMain(WLZ_CONTOUR, dom, val, NULL, NULL, &errNum);
+    }
+    if(errNum != WLZ_ERR_NONE)
+    {
+      WlzFreeContour(ctr);
+      ctr = NULL;
+    }
+  }
+  if(dstErr)
+  {
+    *dstErr = errNum;
+  }
+  return(obj);
+}
+
+/************************************************************************
+* Function:	WlzEffReadGMVtk
+* Returns:	WlzGMModel *:		Object read from file.
+* Purpose:	Reads a WlzGMModel from the given stream
+*		using the Visualization Toolkit (polydata) file
+*		format.
+* Global refs:	-
+* Parameters:	FILE *fP:		Input file stream.
+*		WlzEffVtkHeader *header: Header data structure.
+* 		WlzErrorNum *dstErr:	Destination error number ptr,
+*					may be NULL.
+************************************************************************/
+WlzGMModel	*WlzEffReadGMVtk(FILE *fP, WlzEffVtkHeader *header,
+				 WlzErrorNum *dstErr)
+{
+  int		valI,
+		pIdx,
+		nPoly,
+		nPoints,
+		sumPoints,
+		endOfData,
+		vHTSz;
+  char		*valS;
+  WlzGMModel	*model = NULL;
+  WlzObject	*obj = NULL;
+  WlzEffVtkPolyDataType prim;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  WlzDVertex3	*pointBuf = NULL;
+  WlzDVertex3	triBuf[3];
+  int		polyBuf[3];
+  char		buf[256];
+
+  if(header->dataType == WLZEFF_VTK_DATATYPE_BINARY)
+  {
+    /* Can only read ascii polydata. */
+    errNum = WLZ_ERR_READ_INCOMPLETE;
+  }
+  if(errNum == WLZ_ERR_NONE)
+  {
+    sumPoints = 0;
+    endOfData = 0;
+    do
+    {
+      /* Read line containing token. */
+      if(fgets(buf, 256, fP) == NULL)
+      {
+        endOfData = 1;
+      }
+      else
+      {
+	buf[255] = '\0';
+	valS = strtok(buf, " \t\n");
+      }
+      if(valS)
+      {
+	if((WlzStringMatchValue(&valI, valS,
+		"POINTS", WLZEFF_VTK_POLYDATATYPE_POINTS,
+		"VERTICIES", WLZEFF_VTK_POLYDATATYPE_VERTICIES,
+		"LINES", WLZEFF_VTK_POLYDATATYPE_LINES,
+		"POLYGONS", WLZEFF_VTK_POLYDATATYPE_POLYGONS,
+		"TRIANGLE_STRIPS", WLZEFF_VTK_POLYDATATYPE_TRIANGLE_STRIPS,
+		NULL) == 0))
+	{
+	  errNum = WLZ_ERR_READ_INCOMPLETE;
+	}
+	else
+	{
+	  prim = valI;
+	  switch(prim)
+	  {
+	    case WLZEFF_VTK_POLYDATATYPE_POINTS:
+	      valS = strtok(NULL, " \t\n");
+	      if((valS == NULL) || (sscanf(valS, "%d", &nPoints) != 1) ||
+		 (nPoints <= 0))
+	      {
+	        errNum = WLZ_ERR_READ_INCOMPLETE;
+	      }
+	      else
+	      {
+	        valS = strtok(NULL, " \t\n");
+		if((valS == NULL) || strcmp(valS, "float"))
+		{
+		  errNum = WLZ_ERR_READ_INCOMPLETE;
+		}
+	      }
+	      if(errNum == WLZ_ERR_NONE)
+	      {
+	        if((pointBuf = (WlzDVertex3 *)AlcRealloc(pointBuf,
+			              sizeof(WlzDVertex3) * nPoints)) == NULL)
+	        {
+		  errNum == WLZ_ERR_MEM_ALLOC;
+		}
+	      }
+	      if(errNum == WLZ_ERR_NONE)
+	      {
+	        /* Read nPoints 3D verticies. */
+		pIdx = 0;
+		while((errNum == WLZ_ERR_NONE) && (pIdx < nPoints))
+		{
+		  if((fscanf(fP, "%lg", &((pointBuf + pIdx)->vtX)) != 1) ||
+		     (fscanf(fP, "%lg", &((pointBuf + pIdx)->vtY)) != 1) ||
+		     (fscanf(fP, "%lg", &((pointBuf + pIdx++)->vtZ)) != 1))
+		  {
+		    errNum = WLZ_ERR_READ_INCOMPLETE;
+		  }
+		}
+		sumPoints += nPoints;
+	      }
+	      break;
+	    case WLZEFF_VTK_POLYDATATYPE_POLYGONS:
+	      if(model == NULL)
+	      {
+		vHTSz = (nPoints < 1024)? 1024: nPoints / 3;
+    		model = WlzGMModelNew(WLZ_GMMOD_3D, 0, vHTSz, &errNum);
+	      }
+	      else
+	      {
+	        vHTSz = (sumPoints < 1024)? 1024: sumPoints / 3;
+		if(model->vertexHTSz < vHTSz)
+		{
+		  errNum = WlzGMModelRehashVHT(model, vHTSz);
+		}
+	      }
+	      if(errNum == WLZ_ERR_NONE)
+	      {
+		valS = strtok(NULL, " \t\n");
+		if((valS == NULL) || (sscanf(valS, "%d", &nPoly) != 1) ||
+		   (nPoly < 0))
+		{
+		  errNum = WLZ_ERR_READ_INCOMPLETE;
+		}
+	      }
+	      if(errNum == WLZ_ERR_NONE)
+	      {
+	        valS = strtok(NULL, " \t\n");
+		if((valS == NULL) || (sscanf(valS, "%d", &valI) != 1) ||
+		   (valI < 0) || ((valI / nPoly) != 4))
+	        {
+		  /* Can only use triangles ((valI / nPoly) != (1 + 3)). */
+		  errNum = WLZ_ERR_READ_INCOMPLETE;
+		}
+	      }
+	      if(errNum == WLZ_ERR_NONE)
+	      {
+	        pIdx = 0;
+		while((errNum == WLZ_ERR_NONE) && (pIdx < nPoly))
+		{
+		  if((fscanf(fP, "%d", &valI) != 1) || (valI != 3) ||
+		     (fscanf(fP, "%d %d %d",
+		             polyBuf + 0, polyBuf + 1, polyBuf + 2) != 3) ||
+		     (*(polyBuf + 0) < 0) || (*(polyBuf + 0) >= nPoints) ||
+		     (*(polyBuf + 1) < 0) || (*(polyBuf + 1) >= nPoints) ||
+		     (*(polyBuf + 2) < 0) || (*(polyBuf + 2) >= nPoints))
+		  {
+		    errNum = WLZ_ERR_READ_INCOMPLETE;
+		  }
+		  else
+		  {
+		    /* Add triangle to GM. */
+		    *(triBuf + 0) = *(pointBuf + *(polyBuf + 0));
+		    *(triBuf + 1) = *(pointBuf + *(polyBuf + 1));
+		    *(triBuf + 2) = *(pointBuf + *(polyBuf + 2));
+		    errNum = WlzGMModelConstructSimplex3D(model, triBuf);
+		    ++pIdx;
+		  }
+		}
+	      }
+	      break;
+	    case WLZEFF_VTK_POLYDATATYPE_TRIANGLE_STRIPS:
+	      /* TODO Read triangle strip polydata. */
+	      errNum == WLZ_ERR_READ_INCOMPLETE;
+	      break;
+	    case WLZEFF_VTK_POLYDATATYPE_VERTICIES: /* FALLTHROUGH */
+	    case WLZEFF_VTK_POLYDATATYPE_LINES:     /* FALLTHROUGH */
+	    default:
+	      errNum == WLZ_ERR_READ_INCOMPLETE;
+	      break;
+	  }
+	}
+      }
+    }
+    while((endOfData == 0) && (errNum == WLZ_ERR_NONE));
+  }
+  if(errNum != WLZ_ERR_NONE)
+  {
+    if(model)
+    {
+      (void )WlzGMModelFree(model);
+      model = NULL;
+    }
+  }
+  if(dstErr)
+  {
+    *dstErr = errNum;
+  }
+  return(model);
+}
+
+/************************************************************************
+* Function:	WlzEffReadImgVtk
+* Returns:	WlzObject *:		Object read from file.
+* Purpose:	Reads a Woolz domain object from the given stream
+*		using the Visualization Toolkit (structured points)
+*		file format.
+* Global refs:	-
+* Parameters:	FILE *fP:		Input file stream.
+*		WlzEffVtkHeader *header: Header data structure.
+* 		WlzErrorNum *dstErr:	Destination error number ptr,
+*					may be NULL.
+************************************************************************/
+WlzObject	*WlzEffReadImgVtk(FILE *fP, WlzEffVtkHeader *header,
+				  WlzErrorNum *dstErr)
+{
+  WlzObject	*obj = NULL;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+
+  /* TODO Read VTK images. */
+  errNum = WLZ_ERR_READ_INCOMPLETE;
+
+  if(dstErr)
+  {
+    *dstErr = errNum;
+  }
+  return(obj);
 }
