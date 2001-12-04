5d19989a459fc6b7ab2af47a395085a09dec1949
bill <bill>
2001-12-04 15:07:42 +0000

    Added cutting section through a WlzGMModel.

--- binWlz/Wlz3DGetSection.c
+++ binWlz/Wlz3DGetSection.c
@@ -15,6 +15,7 @@
 ************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 #include <float.h>
 #include <Wlz.h>
@@ -49,14 +50,17 @@ int main(int	argc,
 {
 
   WlzObject	*obj, *nobj;
-  FILE		*inFile;
-  char 		optList[] = "a:d:f:h";
+  FILE		*inFP,
+  		*outFP;
+  char		*outFile;
+  char 		optList[] = "a:d:f:o:h";
   int		option;
   double	dist=0.0, theta=0.0, phi=0.0;
   WlzDVertex3	fixed={0.0,0.0,0.0};
   WlzThreeDViewStruct	*viewStr=NULL;
   WlzErrorNum	errNum=WLZ_ERR_NONE;
     
+  outFile = "-";
   /* read the argument list and check for an input file */
   opterr = 0;
   while( (option = getopt(argc, argv, optList)) != EOF ){
@@ -84,6 +88,9 @@ int main(int	argc,
       }
       break;
 
+    case 'o':
+      outFile = optarg;
+      break;
     case 'h':
     default:
       usage(argv[0]);
@@ -92,20 +99,33 @@ int main(int	argc,
     }
   }
 
-  inFile = stdin;
+  inFP = stdin;
   if( optind < argc ){
-    if( (inFile = fopen(*(argv+optind), "r")) == NULL ){
+    if( (inFP = fopen(*(argv+optind), "r")) == NULL ){
       fprintf(stderr, "%s: can't open file %s\n", argv[0], *(argv+optind));
       usage(argv[0]);
       return 1;
     }
   }
+  if(strcmp(outFile, "_"))
+  {
+    if((outFP = fopen(outFile, "w")) == NULL)
+    {
+      errNum = WLZ_ERR_WRITE_EOF;
+    }
+  }
+  else
+  {
+    outFP = stdout;
+  }
 
   /* read objects and section if possible */
-  while((obj = WlzReadObj(inFile, &errNum)) != NULL) 
+  while((errNum == WLZ_ERR_NONE) &&
+        ((obj = WlzReadObj(inFP, &errNum)) != NULL))
   {
     switch( obj->type )
     {
+      case WLZ_CONTOUR:
       case WLZ_3D_DOMAINOBJ:
 	if( viewStr == NULL ){
 	  viewStr = WlzMake3DViewStruct(WLZ_3D_VIEW_STRUCT, &errNum);
@@ -117,7 +137,7 @@ int main(int	argc,
 	}
 	nobj = WlzGetSectionFromObject(obj, viewStr, &errNum);
 	if( nobj != NULL){
-	  WlzWriteObj(stdout, nobj);
+	  WlzWriteObj(outFP, nobj);
 	}
 	else {
 	  return errNum;
@@ -126,12 +146,16 @@ int main(int	argc,
 	break;
 
       default:
-	WlzWriteObj(stdout, obj);
+	WlzWriteObj(outFP, obj);
 	break;
     }
 
     WlzFreeObj(obj);
   }
+  if(errNum == WLZ_ERR_READ_EOF)
+  {
+    errNum = WLZ_ERR_NONE;
+  }
 
-  return WLZ_ERR_NONE;
+  return errNum;
 }
