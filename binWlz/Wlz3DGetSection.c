43fba430265deea2a54a1e48d61f650fb77abf1a
richard <richard>
2002-01-04 13:27:53 +0000

    Extended options on Wlz3DGetSection to include reading all possible parameters and to read parameters from a bibfile. Thus means a new library has been generated to include utility funbctions for woolz IO.

--- binWlz/Wlz3DGetSection.c
+++ binWlz/Wlz3DGetSection.c
@@ -19,6 +19,8 @@
 #include <limits.h>
 #include <float.h>
 #include <Wlz.h>
+#include <WlzIO.h>
+#include <bibFile.h>
 
 /* externals required by getopt  - not in ANSI C standard */
 #ifdef __STDC__ /* [ */
@@ -31,14 +33,28 @@ extern char     *optarg;
 static void usage(char *proc_str)
 {
   fprintf(stderr,
-	  "Usage:\t%s [-t<theta,phi>] [-f<fx,fy,fz>] [-d<dist>]"
+	  "Usage:\t%s [-a <pitch,yaw[,roll]>] [-f <fx,fy,fz>] [-d <dist>]"
+	  " [-b <parameter bibfile>] [-m <mode>] [-s <scale>]"
+	  " [-o <output file>] [-u<ux,uy,uz>]"
 	  " [<3D object input file>]\n"
 	  "\tGet an arbitrary slice from a 3D object\n"
 	  "\twriting the 2D object to standard output\n"
 	  "\tOptions are:\n"
-	  "\t  -a<theta,phi>      viewing angles in degrees\n"
-	  "\t  -f<fx,fy,fz>       fixed point position\n"
-	  "\t  -d<dist>           distance parameter\n"
+	  "\t  -a<pitch,yaw[,roll]> viewing angles in degrees. If roll\n"
+	  "\t                       is defined then the mode is \"absolute\"\n"
+	  "\t  -b<bibfile>        bibfile defining the view parameters e.g.\n"
+	  "\t                     from MAPaint view or warp input I/O\n"
+	  "\t                     Override all other parameter input\n"
+	  "\t  -f<fx,fy,fz>       fixed point position, default - (0,0,0)\n"
+	  "\t  -d<dist>           distance parameter, default - 0.0\n"
+	  "\t  -m<mode>           viewing mode, possible values:\n"
+	  "\t                       mode = 0 - up-is-up (default)\n"
+	  "\t                       mode = 1 - statue\n"
+	  "\t                       mode = 2 - absolute\n"
+	  "\t  -o<output file>    Output filename, default to stdout\n"
+	  "\t  -s<scale>          Scale factor, default - 1.0\n"
+	  "\t  -u<ux,uy,uz>       Up vector for up-is-up mode.\n"
+	  "\t			  Default: (0,0,-1)\n"
 	  "\t  -h                 Help - prints this usage message\n"
 	  "",
 	  proc_str);
@@ -50,27 +66,46 @@ int main(int	argc,
 {
 
   WlzObject	*obj, *nobj;
-  FILE		*inFP,
-  		*outFP;
-  char		*outFile;
-  char 		optList[] = "a:d:f:o:h";
+  FILE		*inFP, *outFP, *bibFP;
+  char		*outFile, *bibFile;
+  char 		optList[] = "a:b:d:f:m:o:s:u:h";
   int		option;
-  double	dist=0.0, theta=0.0, phi=0.0;
+  int		iVal;
+  double	dist=0.0, pitch=0.0, yaw=0.0, roll=0.0;
+  double	scale=1.0;
   WlzDVertex3	fixed={0.0,0.0,0.0};
+  WlzDVertex3	up={0.0,0.0,-1.0};
   WlzThreeDViewStruct	*viewStr=NULL;
+  WlzThreeDViewMode mode=WLZ_UP_IS_UP_MODE;
   WlzErrorNum	errNum=WLZ_ERR_NONE;
+  BibFileRecord	*bibfileRecord;
+  BibFileError	bibFileErr;
+  char		*errMsg;
     
+  /* additional defaults */
   outFile = "-";
+  bibFile = NULL;
+
   /* read the argument list and check for an input file */
   opterr = 0;
   while( (option = getopt(argc, argv, optList)) != EOF ){
     switch( option ){
 
     case 'a':
-      if( sscanf(optarg, "%lg,%lg", &theta, &phi) < 2 ){
+      switch( sscanf(optarg, "%lg,%lg", &pitch, &yaw, &roll) < 2 ){
+      default:
 	usage(argv[0]);
 	return 1;
+      case 2:
+	break;
+      case 3:
+	mode = WLZ_ZETA_MODE;
+	break;
       }
+      break;
+
+    case 'b':
+      bibFile = optarg;
       break;
 
     case 'd':
@@ -88,18 +123,57 @@ int main(int	argc,
       }
       break;
 
+    case 'm':
+      if( sscanf(optarg, "%d", &iVal) < 1 ){
+	usage(argv[0]);
+	return 1;
+      }
+      else if( mode != WLZ_ZETA_MODE ){
+	switch( iVal ){
+	default:
+	  usage(argv[0]);
+	  return 1;
+	case 0:
+	  mode = WLZ_UP_IS_UP_MODE;
+	  break;
+	case 1:
+	  mode = WLZ_STATUE_MODE;
+	  break;
+	case 2:
+	  mode = WLZ_ZETA_MODE;
+	  break;
+	}
+      }
+      break;
+
     case 'o':
       outFile = optarg;
+      break;
+
+    case 's':
+      if( sscanf(optarg, "%lg", &scale) < 1 ){
+	usage(argv[0]);
+	return 1;
+      }
+      break;
+
+    case 'u':
+      if( sscanf(optarg, "%lg,%lg,%lg", &(up.vtX), &(up.vtY),
+		 &(up.vtZ)) < 3 ){
+	usage(argv[0]);
+	return 1;
+      }
       break;
 
     case 'h':
     default:
       usage(argv[0]);
-      return 1;
+      return 0;
 
     }
   }
 
+  /* check input file/stream */
   inFP = stdin;
   if( optind < argc ){
     if( (inFP = fopen(*(argv+optind), "r")) == NULL ){
@@ -108,6 +182,8 @@ int main(int	argc,
       return 1;
     }
   }
+
+  /* check output file/stream */
   if(strcmp(outFile, "-"))
   {
     if((outFP = fopen(outFile, "w")) == NULL)
@@ -120,6 +196,44 @@ int main(int	argc,
     outFP = stdout;
   }
 
+  /* create view structure */
+  if( viewStr = WlzMake3DViewStruct(WLZ_3D_VIEW_STRUCT, &errNum) ){
+    viewStr->theta = yaw * WLZ_M_PI / 180.0;
+    viewStr->phi = pitch * WLZ_M_PI / 180.0;
+    viewStr->zeta = roll * WLZ_M_PI / 180.0;
+    viewStr->dist = dist;
+    viewStr->fixed = fixed;
+    viewStr->up = up;
+    viewStr->view_mode = mode;
+    viewStr->scale = scale;
+  }
+
+  /* check bibfile - select first section parameters in the file */
+  if((errNum == WLZ_ERR_NONE) && (bibFile != NULL)){
+    if( bibFP = fopen(bibFile, "r") ){
+      bibFileErr = BibFileRecordRead(&bibfileRecord, &errMsg, bibFP);
+      while((bibFileErr == BIBFILE_ER_NONE) &&
+	    (strncmp(bibfileRecord->name, "Wlz3DSectionViewParams", 22))){
+	BibFileRecordFree(&bibfileRecord);
+	bibFileErr = BibFileRecordRead(&bibfileRecord, &errMsg, bibFP);
+      }
+      fclose( bibFP );
+      if( bibFileErr != BIBFILE_ER_NONE ){
+	fprintf(stderr, "%s: error reading bibfile: %s\n", argv[0], errMsg);
+	AlcFree((void *) errMsg);
+	return 1;
+      }
+      parse_Wlz3DSectionViewParams_Record(bibfileRecord, viewStr);
+      BibFileRecordFree(&bibfileRecord);
+    }
+    else {
+      fprintf(stderr, "%s: can't open parameter bibfile %s\n", argv[0],
+	      bibFile);
+      return 1;
+    }
+  }
+  
+
   /* read objects and section if possible */
   while((errNum == WLZ_ERR_NONE) &&
         ((obj = WlzReadObj(inFP, &errNum)) != NULL))
@@ -128,14 +242,7 @@ int main(int	argc,
     {
       case WLZ_CONTOUR:
       case WLZ_3D_DOMAINOBJ:
-	if( viewStr == NULL ){
-	  viewStr = WlzMake3DViewStruct(WLZ_3D_VIEW_STRUCT, &errNum);
-	  viewStr->theta = theta * WLZ_M_PI / 180.0;
-	  viewStr->phi = phi * WLZ_M_PI / 180.0;
-	  viewStr->dist = dist;
-	  viewStr->fixed = fixed;
-	  WlzInit3DViewStruct(viewStr, obj);
-	}
+	WlzInit3DViewStruct(viewStr, obj);
 	nobj = WlzGetSectionFromObject(obj, viewStr, &errNum);
 	if( nobj != NULL){
 	  WlzWriteObj(outFP, nobj);
