a9fd9f255c05b5139cce3634bbe08b2993c09a83
bill <bill>
2002-03-15 14:54:15 +0000

    Added WlzRankObj.c

---
+++ binWlz/WlzRankObj.c
@@ -0,0 +1,172 @@
+#pragma ident "MRC HGU $Id$"
+/*!
+* \file         WlzRank.c
+* \author       Bill Hill
+* \date         March 2002
+* \version      $Id$
+* \note
+*               Copyright
+*               2001 Medical Research Council, UK.
+*               All rights reserved.
+*               All rights reserved.
+* \par Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \brief	Rank filters for woolz objects, these are the
+*		generalization of minimum, maximum and median value
+*		filters.
+* \todo         -
+* \bug          None known.
+*/
+#include <stdio.h>
+#include <float.h>
+#include <limits.h>
+#include <string.h>
+#include <Wlz.h>
+
+
+extern char 	*optarg;
+extern int 	optind,
+		opterr,
+		optopt;
+
+int		main(int argc, char *argv[])
+{
+  int		ok = 1,
+  		option,
+  		usage = 0,
+		rSz = 3;
+  double 	rank = 0.5;
+  FILE		*fP = NULL;
+  char		*inObjFileStr,
+  		*outObjFileStr;
+  const char	*errMsgStr;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  WlzObject	*obj = NULL;
+  static char   optList[] = "ho:r:s:";
+  const char    inObjFileStrDef[] = "-",
+  	        outObjFileStrDef[] = "-";
+
+  opterr = 0;
+  inObjFileStr = (char *)inObjFileStrDef;
+  outObjFileStr = (char *)outObjFileStrDef;
+  while(ok && ((option = getopt(argc, argv, optList)) != EOF))
+  {
+    switch(option)
+    {
+      case 'o':
+        outObjFileStr = optarg;
+	break;
+      case 'r':
+	if((sscanf(optarg, "%lg", &rank) != 1) || (rank < 0.0) || (rank > 1.0))
+	{
+	  usage = 1;
+	  ok = 0;
+	}
+	break;
+      case 's':
+	if((sscanf(optarg, "%d", &rSz) != 1) || (rSz < 0))
+	{
+	  usage = 1;
+	  ok = 0;
+	}
+	break;
+      case 'h':
+      default:
+	usage = 1;
+	ok = 0;
+	break;
+    }
+  }
+  if(ok)
+  {
+    if((inObjFileStr == NULL) || (*inObjFileStr == '\0') ||
+       (outObjFileStr == NULL) || (*outObjFileStr == '\0'))
+    {
+      ok = 0;
+      usage = 1;
+    }
+    if(ok && (optind < argc))
+    {
+      if((optind + 1) != argc)
+      {
+        usage = 1;
+        ok = 0;
+      }
+      else
+      {
+        inObjFileStr = *(argv + optind);
+      }
+    }
+  }
+  if(ok)
+  {
+    if((inObjFileStr == NULL) ||
+       (*inObjFileStr == '\0') ||
+       ((fP = (strcmp(inObjFileStr, "-")?
+              fopen(inObjFileStr, "r"): stdin)) == NULL) ||
+       ((obj = WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL) ||
+       (errNum != WLZ_ERR_NONE))
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: failed to read object from file %s\n",
+                     *argv, inObjFileStr);
+    }
+    if(fP && strcmp(inObjFileStr, "-"))
+    {
+      fclose(fP);
+    }
+  }
+  if(ok)
+  {
+    errNum = WlzRankFilter(obj, rSz, rank);
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr, "%s Failed to rank filter object, %s.\n",
+      		     argv[0],
+		     errMsgStr);
+    }
+  }
+  if(ok)
+  {
+    if((fP = (strcmp(outObjFileStr, "-")?
+	     fopen(outObjFileStr, "w"): stdout)) == NULL)
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+		     "%s: Failed to open output file %s.\n",
+		     argv[0], outObjFileStr);
+    }
+  }
+  if(ok)
+  {
+    errNum = WlzWriteObj(fP, obj);
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+                     "%s: Failed to write output object, %s.\n",
+		     argv[0], errMsgStr);
+    }
+  }
+  (void )WlzFreeObj(obj);
+  if(usage)
+  {
+    fprintf(stderr,
+            "Usage: %s [-h] [-o<output file>] [-r#] [-s#] [<input file>]\n"
+    	    "Rank filters the grey values of a Woolz domain object.\n"
+	    "Options are:\n"
+	    "  -r  Required rank. Range [0.0-1.0] with 0.0 minimum, 0.5\n"
+	    "      median and 1.0 maximum value. Default 0.5.\n"
+	    "  -s  Size of filter region, must be greater than zero.\n"
+	    "      Default 3 for 3x3 region.\n",
+	    argv[0]);
+
+  }
+  return(!ok);
+}
