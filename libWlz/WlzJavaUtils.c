a9632d5dff662d648df50d5dc951a045e5812efd
nickb <nickb>
2002-12-12 17:01:12 +0000

    added WlzSetGreyValues

--- libWlz/WlzJavaUtils.c
+++ libWlz/WlzJavaUtils.c
@@ -26,34 +26,75 @@ int WlzObjGetType(WlzObject *obj) {
 }
 /*---------------------------------*/
 int WlzGetIntervalDomainLine1(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_2D_DOMAINOBJ) return -2;
    return obj->domain.i->line1;
 }
 int WlzGetIntervalDomainLastLn(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_2D_DOMAINOBJ) return -2;
    return obj->domain.i->lastln;
 }
 int WlzGetIntervalDomainKol1(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_2D_DOMAINOBJ) return -2;
    return obj->domain.i->kol1;
 }
 int WlzGetIntervalDomainLastKl(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_2D_DOMAINOBJ) return -2;
    return obj->domain.i->lastkl;
 }
 /*---------------------------------*/
 int WlzGetPlaneDomainPlane1(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_3D_DOMAINOBJ) return -2;
    return obj->domain.p->plane1;
 }
 int WlzGetPlaneDomainLastPl(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_3D_DOMAINOBJ) return -2;
    return obj->domain.p->lastpl;
 }
 int WlzGetPlaneDomainLine1(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_3D_DOMAINOBJ) return -2;
    return obj->domain.p->line1;
 }
 int WlzGetPlaneDomainLastLn(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_3D_DOMAINOBJ) return -2;
    return obj->domain.p->lastln;
 }
 int WlzGetPlaneDomainKol1(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_3D_DOMAINOBJ) return -2;
    return obj->domain.p->kol1;
 }
 int WlzGetPlaneDomainLastKl(WlzObject *obj) {
+   if(obj == NULL) return -1;
+   if(obj->type != WLZ_3D_DOMAINOBJ) return -2;
    return obj->domain.p->lastkl;
 }
 /*---------------------------------*/
+WlzObject *WlzSetGreyValues(WlzObject           *domainObj,
+			   WlzObject           *valuesObj,
+			   WlzErrorNum         *dstErr) {
+
+   WlzObject *retObj = NULL;
+   WlzErrorNum  errNum = WLZ_ERR_NONE;
+
+   if(domainObj->type != WLZ_2D_DOMAINOBJ) return (NULL);
+   if(valuesObj->type != WLZ_2D_DOMAINOBJ) return (NULL);
+
+   retObj = WlzMakeMain(domainObj->type,
+		  domainObj->domain,
+		  valuesObj->values,
+		  NULL,
+		  NULL,
+		  &errNum);
+
+  *dstErr = errNum;
+  return (retObj);
+
+}
