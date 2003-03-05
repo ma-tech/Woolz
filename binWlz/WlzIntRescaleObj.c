be1f70624230067d544068f4c678e41dc0ca10b8
bill <bill>
2003-03-05 17:47:37 +0000

    Added WlzIntRescaleObj binary

---
+++ binWlz/WlzIntRescaleObj.c
@@ -0,0 +1,170 @@
+#pragma ident "MRC HGU $Id$"
+/*!
+* \file         WlzIntRescaleObj.c
+* \author       Bill Hill
+* \date         March 2003
+* \version      $Id$
+* \note
+*               Copyright
+*               2002 Medical Research Council, UK.
+*               All rights reserved.
+*               All rights reserved.
+* \par Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \brief	Woolz filter which rescales an object using an integer
+*		scale factor.
+* \todo         -
+* \bug          None known.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <Wlz.h>
+
+extern int      getopt(int argc, char * const *argv, const char *optstring);
+ 
+extern char     *optarg;
+extern int      optind,
+                opterr,
+                optopt;
+
+int             main(int argc, char **argv)
+{
+  int		expand = 1,
+  		scale = 1,
+  		option,
+  		ok = 1,
+		usage = 0;
+  char		*outObjFileStr,
+  		*inObjFileStr;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  FILE		*fP = NULL;
+  WlzObject	*inObj = NULL,
+  		*outObj = NULL;
+  const char	*errMsg;
+  static char	optList[] = "o:s:ceh",
+		outObjFileStrDef[] = "-",
+  		inObjFileStrDef[] = "-";
+
+  opterr = 0;
+  inObjFileStr = inObjFileStrDef;
+  outObjFileStr = outObjFileStrDef;
+  while(ok && (usage == 0) &&
+  	((option = getopt(argc, argv, optList)) != -1))
+  {
+    switch(option)
+    {
+      case 'c':
+	expand = 0;
+	break;
+      case 'e':
+	expand = 1;
+	break;
+      case 's':
+        if(sscanf(optarg, "%d", &scale) != 1)
+	{
+	  usage = 1;
+	  ok = 0;
+	}
+	break;
+      case 'o':
+        outObjFileStr = optarg;
+	break;
+      case 'h':
+      default:
+	usage = 1;
+	ok = 0;
+	break;
+    }
+  }
+  if((inObjFileStr == NULL) || (*inObjFileStr == '\0') ||
+     (outObjFileStr == NULL) || (*outObjFileStr == '\0'))
+  {
+    ok = 0;
+    usage = 1;
+  }
+  if(ok && (optind < argc))
+  {
+    if((optind + 1) != argc)
+    {
+      usage = 1;
+      ok = 0;
+    }
+    else
+    {
+      inObjFileStr = *(argv + optind);
+    }
+  }
+  if(ok)
+  {
+    errNum = WLZ_ERR_READ_EOF;
+    if((inObjFileStr == NULL) ||
+       (*inObjFileStr == '\0') ||
+       ((fP = (strcmp(inObjFileStr, "-")?
+              fopen(inObjFileStr, "r"): stdin)) == NULL) ||
+       ((inObj= WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL))
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsg);
+      (void )fprintf(stderr,
+                     "%s: failed to read object from file %s (%s).\n",
+                     *argv, inObjFileStr, errMsg);
+    }
+    if(fP && strcmp(inObjFileStr, "-"))
+    {
+      fclose(fP);
+    }
+  }
+  if(ok)
+  {
+    if((outObj = WlzIntRescaleObj(inObj, scale, expand, &errNum)) == NULL)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsg);
+      (void )fprintf(stderr,
+		     "%s: failed to rescale object (%s).\n",
+		     *argv, errMsg);
+    }
+  }
+  if(ok)
+  {
+    errNum = WLZ_ERR_WRITE_EOF;
+    if(((fP = (strcmp(outObjFileStr, "-")?
+              fopen(outObjFileStr, "w"):
+              stdout)) == NULL) ||
+       ((errNum = WlzWriteObj(fP, outObj)) != WLZ_ERR_NONE))
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsg);
+      (void )fprintf(stderr,
+                     "%s: failed to write output object (%s).\n",
+                     *argv, errMsg);
+    }
+    if(fP && strcmp(outObjFileStr, "-"))
+    {
+      fclose(fP);
+    }
+  }
+  (void )WlzFreeObj(inObj);
+  (void )WlzFreeObj(outObj);
+  if(usage)
+  {
+    (void )fprintf(stderr,
+    "Usage: "
+    "%s [-s#] [-c] [-e] [-h] [<in object>]\n"
+    "Rescales a woolz object using an integer scale factor.\n"
+    "Options are:\n"
+    "  -s#   Scale factor (set to %d).\n"
+    "  -c    Compress using 1/scale (%sset).\n"
+    "  -e    Expand using scale (%sset).\n"
+    "  -h    Display this usage information.\n",
+    *argv,
+    scale,
+    (expand == 0) ? "" : "not ",
+    (expand != 0) ? "" : "not ");
+  }
+  return(!ok);
+}
