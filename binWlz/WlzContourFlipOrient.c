628c67e9bd08d14b0d343cae457d37d27ff428f8
bill <bill>
2003-04-23 11:43:39 +0000

    New binary.

---
+++ binWlz/WlzContourFlipOrient.c
@@ -0,0 +1,165 @@
+#pragma ident "MRC HGU $Id$"
+/*!
+* \file         WlzContourFlipOrient.c
+* \author       Bill Hill
+* \date         April 2003
+* \version      $Id$
+* \note
+*               Copyright
+*               2001 Medical Research Council, UK.
+*               All rights reserved.
+*               All rights reserved.
+* \par Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \brief	Filters the geometry of a geometric model.
+* \todo         -
+* \bug          None known.
+*/
+#include <stdio.h>
+#include <float.h>
+#include <limits.h>
+#include <string.h>
+#include <Wlz.h>
+
+extern char 	*optarg;
+extern int 	optind,
+		opterr,
+		optopt;
+
+int		main(int argc, char *argv[])
+{
+  int		ok = 1,
+  		option,
+  		usage = 0;
+  FILE		*fP = NULL;
+  char		*inObjFileStr,
+  		*outObjFileStr;
+  const char	*errMsgStr;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  WlzObject	*obj = NULL;
+  static char   optList[] = "ho:";
+  const char    inObjFileStrDef[] = "-",
+  	        outObjFileStrDef[] = "-";
+
+  opterr = 0;
+  inObjFileStr = (char *)inObjFileStrDef;
+  outObjFileStr = (char *)outObjFileStrDef;
+  while(ok && ((option = getopt(argc, argv, optList)) != EOF))
+  {
+    switch(option)
+    {
+      case 'o':
+        outObjFileStr = optarg;
+	break;
+      case 'h':
+      default:
+	usage = 1;
+	ok = 0;
+	break;
+    }
+  }
+  if(ok)
+  {
+    if((inObjFileStr == NULL) || (*inObjFileStr == '\0') ||
+       (outObjFileStr == NULL) || (*outObjFileStr == '\0'))
+    {
+      ok = 0;
+      usage = 1;
+    }
+    if(ok && (optind < argc))
+    {
+      if((optind + 1) != argc)
+      {
+        usage = 1;
+        ok = 0;
+      }
+      else
+      {
+        inObjFileStr = *(argv + optind);
+      }
+    }
+  }
+  if(ok)
+  {
+    if((inObjFileStr == NULL) ||
+       (*inObjFileStr == '\0') ||
+       ((fP = (strcmp(inObjFileStr, "-")?
+              fopen(inObjFileStr, "r"): stdin)) == NULL) ||
+       ((obj = WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL) ||
+       (errNum != WLZ_ERR_NONE))
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: failed to read object from file %s\n",
+                     *argv, inObjFileStr);
+    }
+    if(fP && strcmp(inObjFileStr, "-"))
+    {
+      fclose(fP);
+    }
+  }
+  if(ok)
+  {
+    if(obj == NULL)
+    {
+      errNum = WLZ_ERR_OBJECT_NULL;
+    }
+    if(obj->type != WLZ_CONTOUR)
+    {
+      errNum = WLZ_ERR_OBJECT_TYPE;
+    }
+    else if(obj->domain.core == NULL)
+    {
+      errNum = WLZ_ERR_DOMAIN_NULL;
+    }
+    else
+    {
+      errNum = WlzGMFilterFlipOrient(obj->domain.ctr->model);
+    }
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+      		     "%s: Failed to flip orientation (%s).\n",
+      		     argv[0],
+		     errMsgStr);
+    }
+  }
+  if(ok)
+  {
+    if((fP = (strcmp(outObjFileStr, "-")?
+	     fopen(outObjFileStr, "w"): stdout)) == NULL)
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+		     "%s: Failed to open output file %s.\n",
+		     argv[0], outObjFileStr);
+    }
+  }
+  if(ok)
+  {
+    errNum = WlzWriteObj(fP, obj);
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+                     "%s: Failed to write output object, %s.\n",
+		     argv[0], errMsgStr);
+    }
+  }
+  (void )WlzFreeObj(obj);
+  if(usage)
+  {
+    (void )fprintf(stderr,
+    "Usage: %s [-h] [-o<output file>] [<input file>]\n"
+    "  -h  Output this usage message.\n"
+    "  -o  Output file name, default is the standard output.\n"
+    "Flips the orientation of a contour's edges if 2D or faces if 3D\n",
+    argv[0]);
+  }
+  return(!ok);
+}
