60bf403b2aa5369c7babb8923a4aad4cdc77c6e2
nickb <nickb>
2003-05-06 13:15:25 +0000

    initial version

---
+++ uk/ac/mrc/hgu/SectionViewer/src/sectionViewer/Dummy.java
@@ -0,0 +1,11 @@
+package sectionViewer;
+import sectionViewer.*;
+
+public class Dummy {
+
+  /*
+    this class exists so that the full path to the section Viewer help system
+    can be determined by reflection, even if SVParent is sub-classed.
+  */
+
+}
