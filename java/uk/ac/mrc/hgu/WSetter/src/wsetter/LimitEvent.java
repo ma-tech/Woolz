acf38b0c7b9e0e745a3f4b25dd9228396878f716
nickb <nickb>
2003-06-16 15:59:08 +0000

    added Javadoc comments

--- java/uk/ac/mrc/hgu/WSetter/src/wsetter/LimitEvent.java
+++ java/uk/ac/mrc/hgu/WSetter/src/wsetter/LimitEvent.java
@@ -4,14 +4,16 @@ import wsetter.*;
 import java.util.EventObject;
 
 /**
- * Event class for WSetter bean.
+ * Event class for WSetter.
+ * Created when WSetter model's limits are changed.
  * @author Nick Burton
+ * @see WSetterModel WSetter
  */
 public class LimitEvent extends EventObject {
 
 //----------------------------------------------------
   /**
-   * Constructor
+   * 
    */
    public LimitEvent(Object source) {
       super(source);
