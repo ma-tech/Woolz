b23f3f0ffbe519d00ffe205f90deb927ce52b2af
nickb <nickb>
2003-08-12 10:56:29 +0000

    added WlzBndJavaUtils.c

--- libWlzBnd/WlzBndProto.h
+++ libWlzBnd/WlzBndProto.h
@@ -31,4 +31,30 @@ extern "C" {
 extern WlzErrorNum		WlzBndGreyInvert(
 				  WlzObject *obj);
 
+/************************************************************************
+* WlzJavaUtils.c							*
+************************************************************************/
+extern int 		        WlzBndObjGetType(WlzObject *obj);
+extern int			WlzBndGetIntervalDomainLine1(WlzObject *obj);
+extern int			WlzBndGetIntervalDomainLastLn(WlzObject *obj);
+extern int			WlzBndGetIntervalDomainKol1(WlzObject *obj);
+extern int			WlzBndGetIntervalDomainLastKl(WlzObject *obj);
+extern int			WlzBndGetPlaneDomainPlane1(WlzObject *obj);
+extern int			WlzBndGetPlaneDomainLastPl(WlzObject *obj);
+extern int			WlzBndGetPlaneDomainLine1(WlzObject *obj);
+extern int			WlzBndGetPlaneDomainLastLn(WlzObject *obj);
+extern int			WlzBndGetPlaneDomainKol1(WlzObject *obj);
+extern int			WlzBndGetPlaneDomainLastKl(WlzObject *obj);
+extern WlzObject	       *WlzBndSetGreyValues(WlzObject *domainObj,
+						WlzObject *valuesObj,
+						WlzErrorNum *dstErr);
+extern WlzAffineTransform      *WlzBndGetAffineTransform(
+						WlzThreeDViewStruct *viewStr,
+						WlzErrorNum *dstErr);
+extern void                     WlzBndGetTransformMatrix(
+                                                WlzIVertex2 *dstSizeArrayDat,
+	                                        double  ***dstArrayDat,
+			                        WlzAffineTransform *trans,
+						WlzErrorNum *dstErr);
+
 #endif	/* !WLZBND_PROTO_H Don't put anything after this line */
