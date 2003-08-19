73d2b6c6ec7345f8db5e648588575f88b46c4969
bill <bill>
2003-08-19 09:51:20 +0000

    *** empty log message ***

--- libAlg/AlgShuffle.c
+++ libAlg/AlgShuffle.c
@@ -23,7 +23,7 @@
 #include <string.h>
 #include <Alg.h>
 
-#if defined (CYGWIN) || defined (DARWIN) || defined (WIN32)
+#if defined (CYGWIN) || defined (DARWIN) || defined (_WIN32)
 #define drand48() (((double) rand()) / RAND_MAX)
 #define srand48(X) (srand((unsigned int) X))
 #define lrand48() ((long) ((((double) rand()) / RAND_MAX) * (1<<31)))
