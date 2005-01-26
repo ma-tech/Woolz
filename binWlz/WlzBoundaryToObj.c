8b17b7c8b8286828c1b4f324f1a81503955cba90
bill <bill>
2005-01-26 12:31:11 +0000

    Added cmd line option for fill method.

--- binWlz/WlzBoundaryToObj.c
+++ binWlz/WlzBoundaryToObj.c
@@ -26,16 +26,17 @@ extern int 	optind, opterr, optopt;
 extern char     *optarg;
 #endif /* __STDC__ ] */
 
-static void usage(char *proc_str)
+static void ShowUsage(char *arg)
 {
   fprintf(stderr,
-	  "Usage:\t%s [-h] [<input file>]\n"
-	  "\tConvert a boundary list woolz object\n"
-	  "\tto the corresponding domain object\n"
-	  "\tOptions are:\n"
-	  "\t  -h        Help - prints this usage message\n"
-	  "",
-	  proc_str);
+	  "Usage: %s [-f s|e|o] [-h] [<input file>]\n"
+	  "Options:\n"
+	  "  -f Sets the boundary fill method to be one of: simple (s),\n"
+	  "     even/odd (e) or vertex (v) fill, default is even/odd.\n"
+	  "  -h Help - prints this usage message\n"
+	  "Converts a boundary list woolz object to a corresponding domain\n"
+	  "using the given fill method.\n",
+	  arg);
   return;
 }
  
@@ -45,48 +46,74 @@ int main(int	argc,
 
   WlzObject	*obj, *nobj;
   FILE		*inFile;
-  char 		optList[] = "h";
-  int		option;
+  char 		optList[] = "f:h";
+  int		option,
+  		usage = 0;
+  WlzPolyFillMode fill = WLZ_EVEN_ODD_FILL;
     
   /* read the argument list and check for an input file */
   opterr = 0;
-  if( (option = getopt(argc, argv, optList)) != EOF ){
-    switch( option ){
-
-    case 'h':
-    default:
-      usage(argv[0]);
-      return 1;
-
+  if((option = getopt(argc, argv, optList)) != EOF)
+  {
+    switch(option)
+    {
+      case 'f':
+	switch(*optarg)
+	{
+	  case 'e':
+	    fill = WLZ_EVEN_ODD_FILL;
+	    break;
+	  case 's':
+	    fill = WLZ_SIMPLE_FILL;
+	    break;
+	  case 'v':
+	    fill = WLZ_VERTEX_FILL;
+	    break;
+	  default:
+	    usage = 1;
+	    break;
+	}
+	break;
+      case 'h': /* FALLTHROUGH */
+      default:
+	usage = 1;
+	break;
     }
+  }
+
+  if(usage)
+  {
+    ShowUsage(argv[0]);
+    return 1;
   }
 
   inFile = stdin;
   if( optind < argc ){
     if( (inFile = fopen(*(argv+optind), "r")) == NULL ){
       fprintf(stderr, "%s: can't open file %s\n", argv[0], *(argv+optind));
-      usage(argv[0]);
+      ShowUsage(argv[0]);
       return 1;
     }
   }
 
-  /* read objects and threshold if possible */
+  /* Read objects and convert. */
   while((obj = WlzAssignObject(WlzReadObj(inFile, NULL), NULL)) != NULL) 
   {
     switch( obj->type )
     {
     case WLZ_BOUNDLIST:
-      if( (nobj = WlzBoundaryToObj(obj, WLZ_EVEN_ODD_FILL,
-      				NULL)) != NULL ){
+      if((nobj = WlzBoundaryToObj(obj, fill, NULL)) != NULL)
+      {
 	(void )WlzWriteObj(stdout, nobj);
 	WlzFreeObj(nobj);
       }
       break;
 
     case WLZ_3D_DOMAINOBJ:
-      if( obj->domain.p->type == WLZ_PLANEDOMAIN_BOUNDLIST ){
-	if( (nobj = WlzBoundaryToObj(obj, WLZ_EVEN_ODD_FILL,
-				    NULL)) != NULL ){
+      if(obj->domain.p->type == WLZ_PLANEDOMAIN_BOUNDLIST)
+      {
+	if((nobj = WlzBoundaryToObj(obj, fill, NULL)) != NULL)
+	{
 	  (void )WlzWriteObj(stdout, nobj);
 	  WlzFreeObj(nobj);
 	}
@@ -103,6 +130,5 @@ int main(int	argc,
 
     WlzFreeObj(obj);
   }
-
   return WLZ_ERR_NONE;
 }
