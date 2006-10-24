b55bbb36337b213e365d572f7af60534b0d3ce79
richard <richard>
2006-10-24 13:22:54 +0000

    Two new commands for random dots and class-map generation.

---
+++ binWlzApp/WlzClassMap.c
@@ -0,0 +1,513 @@
+#if defined(__GNUC__)
+#ident "MRC HGU $Id:"
+#else
+#if defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+#pragma ident "MRC HGU $Id:"
+#else static char _Wl_classMap_c[] = "MRC HGU $Id:";
+#endif
+#endif
+/*!
+* \file         WlzClassMap.c
+* \author       richard <Richard.Baldock@hgu.mrc.ac.uk>
+* \date         Fri Oct 20 13:01:56 2006
+* \version      MRC HGU $Id$
+*               $Revision$
+*               $Name$
+* \par Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par Copyright:
+* Copyright (C) 2005 Medical research Council, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \ingroup      BinWlzApp
+* \brief        Generate a class image from a set of probability
+ images, setting colours from a standard list.
+*               
+* \todo         -
+* \bug          None known
+*
+* Maintenance log with most recent changes at top of list.
+*/
+ 
+/*!
+\ingroup      BinWlzApp
+\defgroup     wlzclassmap WlzClassMap
+\par Name
+WlzClassMap - 
+\par Synopsis
+\verbatim
+WlzClassMap
+
+\endverbatim
+\par Options
+<table width="500" border="0">
+  <tr>
+    <td><b>-</b></td>
+    <td> </td>
+  </tr>
+  <tr>
+    <td><b>-o</b></td>
+    <td>Output object file name.</td>
+  </tr>
+  <tr>
+    <td><b>-h</b></td>
+    <td>Help - print help message</td>
+  </tr>
+  <tr>
+    <td><b>-v</b></td>
+    <td>Verbose operation</td>
+  </tr>
+</table>
+
+\par Description
+
+\par Examples
+\verbatim
+\endverbatim
+
+\par See Also
+\par Bugs
+None known
+*/
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+#include <stdio.h>
+#include <stdlib.h>
+#include <Wlz.h>
+#include <WlzExtFF.h>
+
+/* insert code here */
+/* externals required by getopt  - not in ANSI C standard */
+#ifdef __STDC__ /* [ */
+extern int      getopt(int argc, char * const *argv, const char *optstring);
+ 
+extern int 	optind, opterr, optopt;
+extern char     *optarg;
+#endif /* __STDC__ ] */
+
+static int cmpObjSizeA(
+  const void *p1,
+  const void *p2)
+{
+  WlzObject	**obj1 = (WlzObject **) p1;
+  WlzObject     **obj2 = (WlzObject **) p2;
+  int		size1, size2;
+
+  size1 = WlzArea(*obj1, NULL);
+  size2 = WlzArea(*obj2, NULL);
+
+  if( size1 > size2 ){
+    return 1;
+  }
+  else if( size1 < size2 ){
+    return -1;
+  }
+  return 0;
+}
+
+static int cmpObjSizeB(
+  const void *p1,
+  const void *p2)
+{
+  WlzObject	**obj1 = (WlzObject **) p1;
+  WlzObject     **obj2 = (WlzObject **) p2;
+  int		size1, size2;
+
+  size1 = WlzArea(*obj1, NULL);
+  size2 = WlzArea(*obj2, NULL);
+
+  if( size1 < size2 ){
+    return 1;
+  }
+  else if( size1 > size2 ){
+    return -1;
+  }
+  return 0;
+}
+
+static WlzUByte colorTable[][3] =
+{
+  {255,0,0},
+  {0,255,0},
+  {0,0,255},
+  {255,191,0},
+  {255,0,191},
+  {191,255,0},
+  {0,255,191},
+  {191,0,255},
+  {0,191,255},
+  {255,127,64},
+  {255,64,127},
+  {127,255,64},
+  {64,255,127},
+  {127,64,255},
+  {64,127,255},
+  {255,255,0},
+  {255,0,255},
+  {0,255,255},
+  {255,96,96},
+  {96,255,96},
+  {96,96,255}
+};
+
+static void usage(char *proc_str)
+{
+  fprintf(stderr,
+	  "Usage:\t%s [-b <background col>] [-B <zero col>] [-d <domainfile>]"
+	  "[-i <indexfile>][-n <num>] [-N] [-s] [-S] [-h] [-v] [<input file>]\n"
+	  "\tGenerate a classification map from a series of input image.\n"
+	  "\tEach image is assumed to represent a class \"probability\", scaled\n"
+	  "\tbetween 0 and 255. For each pixel the class with the maximum\n"
+	  "\tprobability value is selected and the colour set appropriately.\n"
+	  "\tOptions are:\n"
+	  "\t  -b r,g,b  Colour for background, i.e. outside of the domain: default (0,0,0)\n"
+	  "\t  -B r,g,b  Colour for zero values  default (0,0,0)\n"
+	  "\t  -d <file> Optional input domain over which class-map\n"
+	  "\t            will be calculated, default - union of input.\n"
+	  "\t  -i <file> Create a colour index strip image (jpeg)\n"
+	  "\t  -n#       Maximum number of objects -default=100\n"
+	  "\t  -N        Normalise the input objects.\n"
+	  "\t  -o        Display an outline of the domain\n"
+	  "\t  -s        Sort on size - smallest first.\n"
+	  "\t  -S        Sort on size - largest first.\n"
+	  "\t  -h        Help - prints this usage message\n"
+	  "\t  -v        Verbose operation\n"
+	  "\n"
+	  "Note colour values are 0-255\n",
+	  proc_str);
+  return;
+}
+
+int main(int	argc,
+	 char	**argv)
+{
+
+  WlzObject	*obj1, *obj2, *obj, **objlist, *rtnObj, *structElem;
+  WlzObjectType	type = (WlzObjectType) -1;
+  int 		i, n, nmax, p;
+  FILE		*inFile;
+  char 		optList[] = "b:B:d:i:n:NosShv";
+  int		option;
+  char 		*indexFile=NULL;
+  int		normFlg=0;
+  int		outlineFlg=0;
+  int		sortFlg=0;
+  int		verboseFlg = 0;
+  WlzPixelV	bckgrnd;
+  WlzPixelV	zeroVal;
+  int		red, green, blue;
+  int		width, height;
+  int		*buf;
+  WlzValues	values;
+/*  WlzUByte	**colorTable; */
+  WlzIntervalWSpace	iwsp;
+  WlzGreyWSpace		gwsp;
+  WlzGreyValueWSpace	**gVWSpArr;
+  const char	*errMsg;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+
+  /* setup the return object type and background */
+  bckgrnd.type = WLZ_GREY_RGBA;
+  bckgrnd.v.rgbv = 0;
+  zeroVal.type = WLZ_GREY_RGBA;
+  zeroVal.v.rgbv = 0;
+    
+  /* read the argument list and check for an input file */
+  opterr = 0;
+  nmax = 100;
+  rtnObj = NULL;
+  obj = NULL;
+  while( (option = getopt(argc, argv, optList)) != EOF ){
+    switch( option ){
+
+    case 'b':
+      if( sscanf(optarg, "%d,%d,%d", &red, &green, &blue) < 3 ){
+	fprintf(stderr, "%s: not enough colour values read in, using default\n",
+		argv[0]);
+      }
+      else {
+	WLZ_RGBA_RGBA_SET(bckgrnd.v.rgbv, red, green, blue, 255);
+      }
+      break;
+
+    case 'B':
+      if( sscanf(optarg, "%d,%d,%d", &red, &green, &blue) < 3 ){
+	fprintf(stderr, "%s: not enough colour values read in, using default\n",
+		argv[0]);
+      }
+      else {
+	WLZ_RGBA_RGBA_SET(zeroVal.v.rgbv, red, green, blue, 255);
+      }
+      break;
+
+      /* read in a target domain over which to capture the occupancy,
+	 set grey type to WLZ_GREY_INT and value to zero */
+    case 'd':
+      if( inFile = fopen(optarg, "rb") ){
+	if( (obj = WlzReadObj(inFile, &errNum)) == NULL ){
+	  fprintf(stderr, "%s: Can't read class-map domain file\n", argv[0]);
+	  usage(argv[0]);
+	}
+	fclose(inFile);
+      }
+      else {
+	fprintf(stderr, "%s: Can't open class-map domain file\n", argv[0]);
+	usage(argv[0]);
+      }
+      break;
+
+    case 'i':
+      indexFile = AlcStrDup(optarg);
+      break;
+
+    case 'n':
+      nmax = atoi(optarg);
+      if( nmax < 1 ){
+	fprintf(stderr, "%s: nmax = %d is invalid\n", argv[0], nmax);
+	usage(argv[0]);
+	return( 1 );
+      }
+      break;
+
+    case 'N':
+      normFlg = 1;
+      break;
+
+    case 'o':
+      outlineFlg = 1;
+      break;
+
+    case 's':
+      sortFlg = 1;
+      break;
+
+    case 'S':
+      sortFlg = 2;
+      break;
+
+    case 'v':
+      verboseFlg = 1;
+      break;
+
+    case 'h':
+    default:
+      usage(argv[0]);
+      return( 1 );
+
+    }
+  }
+
+  /* check for input domain */
+  if( obj ){
+    width = obj->domain.i->lastkl - obj->domain.i->kol1 + 7;
+    height = obj->domain.i->lastln - obj->domain.i->line1 + 7;
+    buf = (int *) AlcMalloc(sizeof(WlzUInt) * width * height);
+    rtnObj = WlzMakeRect(obj->domain.i->line1-3,
+			 obj->domain.i->lastln+3,
+			 obj->domain.i->kol1-3,
+			 obj->domain.i->lastkl+3,
+			 WLZ_GREY_RGBA,
+			 buf,
+			 bckgrnd,
+			 NULL, NULL, &errNum);
+    WlzGreySetValue(rtnObj, bckgrnd);
+    if( outlineFlg ){
+      structElem = WlzMakeCircleObject(2.0, 0.0, 0.0, &errNum);
+      obj2 = WlzStructDilation(obj, structElem, &errNum);
+      obj2->values = WlzAssignValues(rtnObj->values, &errNum);
+      WLZ_RGBA_RGBA_SET(bckgrnd.v.rgbv, 255, 255, 255, 255);
+      WlzGreySetValue(obj2, bckgrnd);
+      WlzFreeObj(obj2);
+      WlzFreeObj(structElem);
+    }
+    obj->values = WlzAssignValues(rtnObj->values, &errNum);
+    WlzGreySetValue(obj, zeroVal);
+    WlzFreeObj(obj);
+    obj = NULL;
+  }
+
+  inFile = stdin;
+  if( optind < argc ){
+    if( (inFile = fopen(*(argv+optind), "rb")) == NULL ){
+      fprintf(stderr, "%s: can't open file %s\n", argv[0], *(argv+optind));
+      usage(argv[0]);
+      return( 1 );
+    }
+  }
+
+  /* allocate space for the object pointers */
+  if( (objlist = (WlzObject **)
+       AlcMalloc(sizeof(WlzObject *) * nmax)) == NULL ){
+    (void )fprintf(stderr, "%s: memory allocation failed.\n",
+    		   argv[0]);
+    return( 1 );
+  }
+
+  /* read objects accumulating compatible types */
+  n = 0;
+  while( ((obj = WlzAssignObject(WlzReadObj(inFile, NULL),
+  			         NULL)) != NULL) && (n < nmax) ) {
+
+    if( type == -1 &&
+	(obj->type == WLZ_2D_DOMAINOBJ) ){
+      type = obj->type;
+    }
+
+    if( obj->type == type ){
+      objlist[n++] = WlzAssignObject(obj, NULL);
+    } else {
+      WlzFreeObj( obj );
+    }
+  }
+
+  if( type == WLZ_EMPTY_OBJ ){
+    return( 0 );
+  }
+
+  /* sort on size - area or volume */
+  if( sortFlg == 1 ){
+    /* smallest first */
+    qsort(objlist, n, sizeof(WlzObject *), cmpObjSizeA);
+  }
+  else if ( sortFlg == 2 ){
+    /* largest first */
+    qsort(objlist, n, sizeof(WlzObject *), cmpObjSizeB);
+  }
+
+  /* check for class-map object */
+  if( rtnObj ){
+    /* check type against return object - must be the same */
+    if( type != rtnObj->type ){
+      fprintf(stderr, "%s: Class-map domain object type does not match\n"
+	      " input domain type\n", argv[0]);
+      usage(argv[0]);
+      return 1;
+    }
+  }
+  else {
+    /* use the union of input domains as the occupancy object */
+    if((obj1 = WlzUnionN(n, objlist, 0, &errNum)) == NULL) {
+      (void )WlzStringFromErrorNum(errNum, &errMsg);
+      (void )fprintf(stderr, "%s: failed to perform union (%s).\n",
+		     argv[0], errMsg);
+      return(1);
+    }
+    width = obj1->domain.i->lastkl - obj1->domain.i->kol1 + 7;
+    height = obj1->domain.i->lastln - obj1->domain.i->line1 + 7;
+    buf = (int *) AlcMalloc(sizeof(WlzUInt) * width * height);
+    rtnObj = WlzMakeRect(obj1->domain.i->line1-3,
+			 obj1->domain.i->lastln+3,
+			 obj1->domain.i->kol1-3,
+			 obj1->domain.i->lastkl+3,
+			 WLZ_GREY_RGBA,
+			 buf,
+			 bckgrnd,
+			 NULL, NULL, &errNum);
+    WlzGreySetValue(rtnObj, bckgrnd);
+    if( outlineFlg ){
+      structElem = WlzMakeCircleObject(2.0, 0.0, 0.0, &errNum);
+      obj2 = WlzStructDilation(obj1, structElem, &errNum);
+      obj2->values = WlzAssignValues(rtnObj->values, &errNum);
+      WLZ_RGBA_RGBA_SET(bckgrnd.v.rgbv, 255, 255, 255, 255);
+      WlzGreySetValue(obj2, bckgrnd);
+      WlzFreeObj(obj2);
+      WlzFreeObj(structElem);
+    }
+    obj1->values = WlzAssignValues(rtnObj->values, &errNum);
+    WlzGreySetValue(obj1, zeroVal);
+    WlzFreeObj(obj1);
+  }
+
+  /* set up a colour table */
+/*  AlcUnchar2Malloc(&colorTable, n, 3);*/
+/* use static table until we can work out a decent algorithm */
+  
+
+  /* Find class for each pixel and set colour */
+  errNum = WlzInitGreyScan(rtnObj, &iwsp, &gwsp);
+  gVWSpArr = (WlzGreyValueWSpace **) AlcMalloc(sizeof(WlzGreyValueWSpace *) * n);
+  for(i=0; i < n; i++){
+    gVWSpArr[i] = WlzGreyValueMakeWSp(objlist[i], &errNum);
+  }
+  while( (errNum = WlzNextGreyInterval(&iwsp)) == WLZ_ERR_NONE ){
+    WlzGreyP		gptr;
+    int	k, max, class;
+    gptr = gwsp.u_grintptr;
+    for(k=iwsp.lftpos; k <= iwsp.rgtpos; k++, gptr.rgbp++){
+      max = 0;
+      class = -1;
+      for(i=0; i < n; i++){
+	if( WlzInsideDomain(objlist[i],0,iwsp.linpos, k, &errNum) ){
+	  WlzGreyValueGet(gVWSpArr[i], 0, iwsp.linpos, k);
+	  if( gVWSpArr[i]->gVal[0].inv > max ){
+	    max = gVWSpArr[i]->gVal[0].inv;
+	    class = i;
+	  }
+	}
+      }
+      if( class > -1 ){
+	WLZ_RGBA_RGBA_SET(*gptr.rgbp,
+			  colorTable[class][0],
+			  colorTable[class][1],
+			  colorTable[class][2],
+			  255);
+      }
+    }
+  }
+  if( errNum == WLZ_ERR_EOO ){
+    errNum = WLZ_ERR_NONE;
+  }
+
+  
+
+  /* output class-map object object */
+  WlzWriteObj(stdout, rtnObj);
+
+  /* check for index object */
+  if( indexFile ){
+    FILE	*fp;
+    WlzUInt	uiVal;
+    int		k;
+    if( fp = fopen(indexFile, "wb") ){
+      WlzFreeObj(rtnObj);
+      bckgrnd.type = WLZ_GREY_RGBA;
+      bckgrnd.v.rgbv = 0;
+      width = 25;
+      height = 25 * n;
+      buf = (int *) AlcMalloc(sizeof(WlzUInt) * width * height);
+      rtnObj = WlzMakeRect(0, height-1, 0, width-1,
+			   WLZ_GREY_RGBA, buf, bckgrnd,
+			   NULL, NULL, &errNum);
+      for(i=0; i < n; i++){
+	WLZ_RGBA_RGBA_SET(uiVal,
+			  colorTable[i][0],
+			  colorTable[i][1],
+			  colorTable[i][2],
+			  255);
+	for(k=0; k < (width*width); k++, buf++){
+	  *((WlzUInt *) buf) = uiVal;
+	}
+      }
+      WlzEffWriteObj(fp, NULL, rtnObj, WLZEFF_FORMAT_JPEG);
+      fclose(fp);
+    }
+  }
+
+  return( 0 );
+}
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
---
+++ binWlzApp/WlzCreateRandomDomain.c
@@ -0,0 +1,317 @@
+#if defined(__GNUC__)
+#ident "MRC HGU $Id:"
+#else
+#if defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+#pragma ident "MRC HGU $Id:"
+#else static char _WlzCreateRandomDomain_c[] = "MRC HGU $Id:";
+#endif
+#endif
+/*!
+* \file         WlzCreateRandomDomain.c
+* \author       richard <Richard.Baldock@hgu.mrc.ac.uk>
+* \date         Tue Aug 29 09:02:27 2006
+* \version      MRC HGU $Id$
+*               $Revision$
+*               $Name$
+* \par Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par Copyright:
+* Copyright (C) 2005 Medical research Council, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \ingroup      WlzBinApp
+* \brief        Generate a set of random domains to cover a given model domain.
+*               
+* \todo         -
+* \bug          None known
+*
+* Maintenance log with most recent changes at top of list.
+*/
+ 
+/*!
+\ingroup      WlzBinApp
+\defgroup     wlzcreaterandomdomain WlzCreateRandomDomain
+\par Name
+WlzCreateRandomDomain - 
+\par Synopsis
+\verbatim
+WlzCreateRandomDomain
+
+\endverbatim
+\par Options
+<table width="500" border="0">
+  <tr>
+    <td><b>-</b></td>
+    <td> </td>
+  </tr>
+  <tr>
+    <td><b>-o</b></td>
+    <td>Output object file name.</td>
+  </tr>
+  <tr>
+    <td><b>-h</b></td>
+    <td>Help - print help message</td>
+  </tr>
+  <tr>
+    <td><b>-v</b></td>
+    <td>Verbose operation</td>
+  </tr>
+</table>
+
+\par Description
+
+\par Examples
+\verbatim
+\endverbatim
+
+\par See Also
+\par Bugs
+None known
+*/
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <Wlz.h>
+
+/* externals required by getopt  - not in ANSI C standard */
+#ifdef __STDC__ /* [ */
+extern int      getopt(int argc, char * const *argv, const char *optstring);
+ 
+extern int 	optind, opterr, optopt;
+extern char     *optarg;
+#endif /* __STDC__ ] */
+
+static void usage(
+  char	*str)
+{
+  fprintf(stderr,
+	  "Usage:\n"
+	  "%s -n <num domains> -r <radius> -t <type> -h -v  [<model-file>]\n"
+	  "\tRead in model domain from file or stdin and generate a set of random\n"
+	  "\tdomains within the model. <num domains> domains are created\n"
+	  "\taccording to type and written to stdout.\n"
+	  "Arguments:\n"
+	  "\t-n#        number of domains to be generate (default 100)\n"
+	  "\t-r#        radius parameter for random domains (default 100)\n"
+	  "\t-t#        type of random domain default 1\n"
+	  "\t             = 1 - circular domain (on a single plane in 3D)\n"
+	  "\t             = 2 - spherical domain\n"
+	  "\t-h         print this message\n"
+	  "\t-v         verbose operation\n"
+	  "\n",
+	  str);
+
+  return;
+}
+
+static int WlzSize(
+  WlzObject	*obj,
+  WlzErrorNum	*dstErr)
+{
+  int	size;
+  WlzErrorNum	errNum=WLZ_ERR_NONE;
+
+  if( obj ){
+    switch(obj->type){
+    case WLZ_2D_DOMAINOBJ:
+      size = WlzArea(obj, &errNum);
+      break;
+
+    case WLZ_3D_DOMAINOBJ:
+      size = WlzVolume(obj, &errNum);
+      break;
+
+    case WLZ_EMPTY_OBJ:
+      size = 0;
+      break;
+
+    default:
+      size = -1;
+      errNum = WLZ_ERR_OBJECT_TYPE;
+      break;
+    }
+  }
+  else {
+    size = -1;
+    errNum = WLZ_ERR_OBJECT_NULL;
+  }
+
+  if( dstErr ){
+    *dstErr = errNum;
+  }
+  return size;
+}
+
+
+int main(
+  int   argc,
+  char  **argv)
+{
+  FILE		*inFile;
+  char 		optList[] = "n:r:t:hv";
+  int		option;
+  WlzErrorNum	errNum=WLZ_ERR_NONE;
+  char		*errMsg;
+  int		verboseFlg=0;
+  int		type=1;
+  int		numDomains=100, domainCount;
+  double	radius=5.0;
+  int		minSize;
+  WlzObject	*obj, *baseDomain, *obj1, *obj2;
+  WlzDomain	domain;
+  WlzValues	values;
+
+  /* read the argument list and check for an input file */
+  opterr = 0;
+  while( (option = getopt(argc, argv, optList)) != EOF ){
+    switch( option ){
+
+    case 'n':
+      numDomains = atoi(optarg);
+      if( numDomains < 1 ){
+	fprintf(stderr, "%s: number of domains requested\n", argv[0]);
+	return 1;
+      }
+      if( numDomains > 10000 ){
+	fprintf(stderr, "%s: %d domains requested, are your sure? Quit with <ctrl> c\n",
+		argv[0], numDomains);
+      }
+      break;
+
+    case 'r':
+      radius = atof(optarg);
+      if( radius <= 1.0){
+	radius = 5.0;
+	fprintf(stderr, "%s: invalid radius, reset to 5\n", argv[0]);
+      }
+      break;
+
+    case 't':
+      type = atoi(optarg);
+      break;
+
+    case 'v':
+      verboseFlg = 1;
+      break;
+
+    case 'h':
+    default:
+      usage(argv[0]);
+      return 1;
+
+    }
+  }
+
+  /* get the target domain */
+  inFile = stdin;
+  if( optind < argc ){
+    if( (inFile = fopen(*(argv+optind), "rb")) == NULL ){
+      fprintf(stderr, "%s: can't open file %s\n", argv[0], *(argv+optind));
+      usage(argv[0]);
+      return( 1 );
+    }
+  }
+  if( (obj = WlzReadObj(inFile, &errNum)) == NULL ){
+    fprintf(stderr, "%s: failed to read a woolz object from %s\n",
+	    argv[0], *(argv+optind));
+    usage(argv[0]);
+    return( 1 );
+  }
+
+  /* generate the base domain */
+  switch( obj->type ){
+  case WLZ_2D_DOMAINOBJ:
+    baseDomain = WlzMakeCircleObject(radius, 0.0, 0.0, &errNum);
+    break;
+
+  case WLZ_3D_DOMAINOBJ:
+    switch( type ){
+    case 1:
+      obj1 = WlzMakeCircleObject(radius, 0.0, 0.0, &errNum);
+      domain.p = WlzMakePlaneDomain(WLZ_PLANEDOMAIN_DOMAIN,
+				    0, 0, 0, 0, 0, 0, &errNum);
+      domain.p->domains[0] = WlzAssignDomain(obj1->domain, &errNum);
+      values.core = NULL;
+      baseDomain = WlzMakeMain(WLZ_3D_DOMAINOBJ, domain, values,	
+			       NULL, NULL, &errNum);
+      WlzFreeObj(obj1);
+      break;
+
+    case 2:
+      baseDomain = WlzMakeSphereObject(WLZ_3D_DOMAINOBJ, radius, 0.0, 0.0, 0.0, &errNum);
+      break;
+
+    default:
+      fprintf(stderr, "%s: invalid random domain type: %d\n",
+	      argv[0], type);
+      return 1;
+    }
+    break;
+
+  default:
+    fprintf(stderr, "%s: invalid object type from %s, must be a 2D or 3D domain object\n",
+	    argv[0], *(argv+optind));
+    return 1;
+  }
+  minSize = WlzSize(baseDomain, &errNum) / 4;
+
+  /* now generate random domains */
+  domainCount= 0;
+  srandomdev();
+  while( domainCount < numDomains ){
+    long	xRan, yRan, zRan;
+    double	xp, yp, zp;
+    int		x, y, z;
+    xRan = random();
+    yRan = random();
+    zRan = random();
+    xp = ((double) (xRan&0xffffff))/0xffffff;
+    yp = ((double) (yRan&0xffffff))/0xffffff;
+    zp = ((double) (zRan&0xffffff))/0xffffff;
+
+    switch( obj->type ){
+    case WLZ_2D_DOMAINOBJ:
+      x = obj->domain.i->kol1 + xp*(obj->domain.i->lastkl - obj->domain.i->kol1);
+      y = obj->domain.i->line1 + yp*(obj->domain.i->lastln - obj->domain.i->line1);
+      z = 0;
+      break;
+
+    case WLZ_3D_DOMAINOBJ:
+      x = obj->domain.p->kol1 + xp*(obj->domain.p->lastkl - obj->domain.p->kol1);
+      y = obj->domain.p->line1 + yp*(obj->domain.p->lastln - obj->domain.p->line1);
+      z = obj->domain.p->plane1 + zp*(obj->domain.p->lastpl - obj->domain.p->plane1);
+      break;
+    }
+    if( obj1 = WlzShiftObject(baseDomain, x, y, z, &errNum) ){
+      if( obj2 = WlzIntersect2(obj, obj1, &errNum) ){
+	if( WlzSize(obj2, &errNum) > minSize ){
+	  WlzWriteObj(stdout, obj2);
+	  WlzFreeObj(obj2);
+	  domainCount++;
+	}
+      }
+      WlzFreeObj(obj1);
+    }
+  }
+
+  return 0;
+}
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
