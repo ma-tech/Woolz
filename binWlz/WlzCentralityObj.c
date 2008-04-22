72a384908f5180404d0bb7db6bd1ef03951c096f
bill <bill>
2008-04-22 16:09:15 +0000

    New file.

---
+++ binWlz/WlzCentralityObj.c
@@ -0,0 +1,279 @@
+#if defined(__GNUC__)
+#ident "MRC HGU $Id$"
+#else
+#if defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+#pragma ident "MRC HGU $Id$"
+#else
+static char _WlzCentralityObj_c[] = "MRC HGU $Id$";
+#endif
+#endif
+/*!
+* \file         WlzCentralityObj.c
+* \author       Bill Hill
+* \date         April 2008
+* \version      $Id$
+* \par
+* Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par
+* Copyright (C) 2008 Medical research Council, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \brief	Computes the centrality of a feature object's domain
+* 		with respect to a reference object's domain.
+* \ingroup	BinWlz
+* \todo         -
+* \bug          None known.
+* \par Binary
+* \ref wlzcentralityobj "WlzCentralityObj"
+*/
+
+/*!
+\ingroup BinWlz
+\defgroup wlzcentralityobj WlzCentralityObj
+\par Name
+WlzCentralityObj - computes the centrality of a feature object's domain
+		   with respect to a reference object's domain.
+\par Synopsis
+\verbatim
+WlzCentralityObj [-b] [-h] [-n#] [-o <output file>] [<feat obj>] [<ref obj>]
+\endverbatim
+\par Options
+<table width="500" border="0">
+  <tr> 
+    <td><b>-b</b></td>
+    <td>Treat the feature object as a binary object with constant values.</td>
+  </tr>
+  <tr> 
+    <td><b>-h</b></td>
+    <td>Help - prints a usage message.</td>
+  </tr>
+  <tr> 
+    <td><b>-n</b></td>
+    <td>Number of angle increments to use (default 360).</td>
+  </tr>
+  <tr> 
+    <td><b>-o</b></td>
+    <td>Output file.</td>
+  </tr>
+</table>
+\par Description
+Computes the centrality of the feature object's domain with respect to
+the reference object's domain.
+By default the feature and reference objects are read from the standard
+input. This may be made explicit if only one of these is given by using
+- to specify the standard input.
+\par Examples
+\verbatim
+WlzCentralityObj -b spots.wlz fish.wlz
+\endverbatim
+Computes the centrality of the domain spots.wlz with respect to the reference
+domain fish.wlz. The sports domain is treated as a binary domain (values are
+ignored).
+\par File
+\ref WlzCentralityObj.c "WlzCentralityObj.c"
+\par See Also
+\ref BinWlz "WlzIntro(1)"
+\ref WlzCentrality "WlzCentrality(3)"
+*/
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Wlz.h>
+
+extern int      getopt(int argc, char * const *argv, const char *optstring);
+ 
+extern char     *optarg;
+extern int 	optind,
+		opterr,
+		optopt;
+
+int		main(int argc, char *argv[])
+{
+  int		ok,
+		binFlg = 0,
+  		nAng = 360,
+  		option,
+		usage = 0;
+  double	cen = 0.0;
+  char		*fFileStr,
+  		*rFileStr,
+		*oFileStr;
+  FILE		*fP = NULL;
+  WlzObject	*fObj = NULL,
+		*rObj = NULL;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  const char	*errMsgStr;
+  static char	optList[] = "bhn:o:";
+  const char    fileStrDef[] = "-";
+
+  /* Parse the argument list and check for input files. */
+  opterr = 0;
+  fFileStr = (char *)fileStrDef;
+  rFileStr = (char *)fileStrDef;
+  oFileStr = (char *)fileStrDef;
+  while((option = getopt(argc, argv, optList)) != EOF)
+  {
+    switch(option)
+    {
+      case 'b':
+        binFlg = 1;
+	break;
+      case 'n':
+	if((sscanf(optarg, "%d", &nAng) != 1) || (nAng <= 0))
+	{
+	  usage = 1;
+	}
+	break;
+      case 'o':
+	oFileStr = optarg;
+	break;
+      case 'h':
+      default:
+	usage = 1;
+	break;
+    }
+  }
+  if(ok && (optind < argc))
+  {
+    if((optind + 1) == argc)
+    {
+      fFileStr = argv[optind];
+    }
+    else if((optind + 2) == argc)
+    {
+      fFileStr = argv[optind];
+      rFileStr = argv[optind + 1];
+    }
+    else
+    {
+      usage = 1;
+    }
+  }
+  ok = !usage;
+  /* Read the feature object. */
+  if(ok)
+  {
+    if((fFileStr == NULL) ||
+       (*fFileStr == '\0') ||
+       ((fP = (strcmp(fFileStr, "-")?
+              fopen(fFileStr, "r"): stdin)) == NULL) ||
+       ((fObj = WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL))
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: Failed to read feature object from file %s.\n",
+                     argv[0], fFileStr);
+    }
+    if(fP && strcmp(fFileStr, "-"))
+    {
+      (void )fclose(fP); fP = NULL;
+    }
+  }
+  /* Read the reference object. */
+  if(ok)
+  {
+    if((rFileStr == NULL) ||
+       (*rFileStr == '\0') ||
+       ((fP = (strcmp(rFileStr, "-")?
+              fopen(rFileStr, "r"): stdin)) == NULL) ||
+       ((rObj = WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL))
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: Failed to read reference object from file %s.\n",
+                     argv[0], rFileStr);
+    }
+    if(fP && strcmp(rFileStr, "-"))
+    {
+      (void )fclose(fP); fP = NULL;
+    }
+  }
+  /* Check object types and parse the promote the default distance function
+   * if required. */
+  if(ok)
+  {
+    if(rObj->type != fObj->type)
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: Feature and reference object types differ.\n",
+		     argv[0]);
+    }
+  }
+  /* Compute centrality feature. */
+  if(ok)
+  {
+    cen = WlzCentrality(fObj, rObj, nAng, binFlg, &errNum);
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+                     "%s: Failed to compute centrality feature value (%s).\n",
+                     argv[0], errMsgStr);
+    }
+  }
+  /* Output the centrality feature value. */
+  if(ok)
+  {
+    if((fP = (strcmp(oFileStr, "-")?
+             fopen(oFileStr, "w"): stdout)) == NULL)
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: Failed to open output file %s.\n",
+                     argv[0], oFileStr);
+    }
+  }
+  if(ok)
+  {
+    if(fprintf(fP, "%lg\n", cen) <= 0)
+    {
+      ok = 0;
+      errNum = WLZ_ERR_WRITE_INCOMPLETE;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+                     "%s: Failed to write output feature value (%s).\n",
+                     argv[0], errMsgStr);
+    }
+  }
+  (void )WlzFreeObj(fObj);
+  (void )WlzFreeObj(rObj);
+  if(usage)
+  {
+    (void )fprintf(stderr,
+    "Usage: %s [-b] [-h] [-n#] [-o <output file>] [<feat obj>] [<ref obj>]\n"
+    "Computes the centrality of the feature object's domain with respect to\n"
+    "the reference object's domain.\n"
+    "Options are:\n"
+    "  -b  Treat the feature object as a binary object with constant values.\n"
+    "  -h  Help - prints this usage message\n"
+    "  -n  Number of angle increments to use (default 360).\n"
+    "  -o  Output file for centrality feature value.\n"
+    "By default the feature and reference objects are read from the standard\n"
+    "input. This may be made explicit if only one of these is given by using\n"
+    "- to specify the standard input.\n",
+    argv[0]);
+  }
+  return(!ok);
+}
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
---
+++ libWlz/WlzCentrality.c
@@ -0,0 +1,452 @@
+#if defined(__GNUC__)
+#ident "MRC HGU $Id$"
+#else
+#if defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+#pragma ident "MRC HGU $Id$"
+#else
+static char _WlzCentrality_c[] = "MRC HGU $Id$";
+#endif
+#endif
+/*!
+* \file         WlzCentrality.c
+* \author       Bill Hill
+* \date         April 2008
+* \version      $Id$
+* \par
+* Address:
+*               MRC Human Genetics Unit,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par
+* Copyright (C) 2008 Medical research Council, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \brief	Functions for computing  the centrality of a feature
+* 		domain with respect to a boundary domain. See
+* 		WlzCentrality().
+* \ingroup	WlzFeatures
+* \todo         -
+* \bug          None known.
+*/
+
+#include <math.h>
+#include <float.h>
+#include <Wlz.h>
+
+
+static double			WlzCentrality2D(
+				  WlzObject *fObj,
+				  WlzObject *bObj,
+				  int nRay,
+				  int binFlg,
+				  WlzErrorNum *dstErr);
+static void			WlzCentralityCompPolarTbl2D(
+				  WlzDVertex2 cmV,
+				  int pTblSz,
+				  double *pTbl,
+				  WlzObject *bndObj);
+static void			WlzCentralityCompPolarTblPly2D(
+				  WlzDVertex2 cmV,
+				  int pTblSz,
+				  double *pTbl,
+				  WlzPolygonDomain *ply);;
+static void			WlzCentralityCompPolarTblBnd2D(
+				  WlzDVertex2 cmV,
+				  int pTblSz,
+				  double *pTbl,
+				  WlzBoundList  *bndLst);
+static void			WlzCentralityUpdate2D(
+				  double *fNum,
+				  double *fDnm,
+				  WlzDVertex2 cmV,
+				  int pTblSz,
+				  double *pTbl,
+				  double mass,
+				  WlzIVertex2 pos);
+
+/*!
+* \return	Centrality feature value in range [0.0-1.0].
+* \ingroup	WlzFeatures
+* \brief	Computes the centrality of a feature domain with respect
+* 		to a boundary domain. The boundary domain must enclose
+* 		the feature domain.
+* 		
+* 		Rays are projected from the centre of mass of the boundary
+* 		domain to it's boundary. The maximum value of this distance
+* 		for a given angle \f(i\f) being \f$R_i\f$. At each point
+* 		in the feature domain intersected by the ray the currrent
+* 		value of the centrality is updated. The distance from the
+* 		centre of mass to each feature point is defined to be
+* 		\f$r_{i,j}\f$ and the mass at this point \f$m_{i,j}\f$.
+* 		The centrality of the feature domain \f$\Omega_f\f$ with
+* 		respect to the border domain \fOmega_b\f$ is defined to be
+* 		\f[
+		c = \frac{({\sum_{i,j}{|m_{i,j}(R_i - r_{i,j)})|}})^2}
+		         {({\sum_{i,j}{m_{i,j}R}})^2}
+                \f]
+* \param	fObj			Feature domain object, \f$Omega_f\f$.
+* \param	bObj			Boundary domain object, \f$Omega_b\f$.
+* \param	nRay			Number of equally spaced rays
+* 					projected from the centre of mass.
+* \param	binFlg			Treat as binary object if non-zero,
+* 					with all masses having value 1.0.
+* \param	dstErr			Destination error pointer, may be NULL.
+*/
+double		WlzCentrality(WlzObject *fObj, WlzObject *bObj,
+			      int nRay, int binFlg,
+			      WlzErrorNum *dstErr)
+{
+  double	cent = 0.0;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+
+  if((fObj == NULL) || (bObj == NULL))
+  {
+    errNum = WLZ_ERR_OBJECT_NULL;
+  }
+  else if(fObj->type != bObj->type)
+  {
+    errNum = WLZ_ERR_OBJECT_TYPE;
+  }
+  else if((fObj->domain.core == NULL) || (bObj->domain.core == NULL))
+  {
+    errNum = WLZ_ERR_DOMAIN_NULL;
+  }
+  else if((binFlg == 0) && (fObj->values.core == NULL))
+  {
+    errNum = WLZ_ERR_VALUES_NULL;
+  }
+  else
+  {
+    switch(fObj->type)
+    {
+      case WLZ_2D_DOMAINOBJ:
+	cent = WlzCentrality2D(fObj, bObj, nRay, binFlg, &errNum);
+        break;
+      case WLZ_3D_DOMAINOBJ:
+	errNum = WLZ_ERR_UNIMPLEMENTED;
+        break;
+      default:
+        errNum = WLZ_ERR_NONE;
+	break;
+    }
+  }
+  if(dstErr)
+  {
+    *dstErr = errNum;
+  }
+  return(cent);
+}
+
+/*!
+* \return	Centrality feature value in range [0.0-1.0].
+* \ingroup	WlzFeatures
+* \brief	Computes the centrality of a feature domain with respect
+* 		to a boundary domain, where the domains are 2D. See
+* 		WlzCentrality().
+* \param	fObj			Feature domain object.
+* \param	bObj			Boundary domain object.
+* \param	nRay			Number of equally spaced rays projected
+* 					from the centre of mass.
+* \param	binFlg			Treat as binary object if non-zero.
+* \param	dstErr			Destination error pointer, may be NULL.
+*/
+static double	WlzCentrality2D(WlzObject *fObj, WlzObject *bObj,
+				int nRay, int binFlg,
+				WlzErrorNum *dstErr)
+{
+  double	cent = 0.0,
+  		fNum = 0.0,
+		fDnm = 0.0;
+  WlzIVertex2	pos;
+  WlzDVertex2	cmV;
+  double	*pTbl = NULL;
+  WlzObject	*tmpObj,
+		*bndObj = NULL;
+  WlzGreyP	gPix;
+  WlzGreyWSpace gWsp;
+  WlzIntervalWSpace iWsp;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+
+  /* Get centre of mass of the boundary object. */
+  cmV = WlzCentreOfMass2D(bObj, 1, NULL, &errNum);
+  /* Get boundary of the boundary domain. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    bndObj = WlzObjToBoundary(bObj, 0, &errNum);
+  }
+  /* Make sure the boundary is 8-connected. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    tmpObj = WlzBoundTo8Bound(bndObj, &errNum);
+    (void )WlzFreeObj(bndObj);
+    bndObj = tmpObj;
+  }
+  /* Compute polar maximum boundary table. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    if((pTbl = (double *)AlcMalloc(nRay * sizeof(double))) == NULL)
+    {
+      errNum = WLZ_ERR_MEM_ALLOC;
+    }
+  }
+  if(errNum == WLZ_ERR_NONE)
+  {
+    WlzCentralityCompPolarTbl2D(cmV, nRay, pTbl, bndObj);
+  }
+  (void )WlzFreeObj(bndObj);
+  /* Scan through feature domain adding to feature values. */
+  if(errNum == WLZ_ERR_NONE)
+  {
+    if(binFlg != 0)
+    {
+      errNum = WlzInitRasterScan(fObj, &iWsp, WLZ_RASTERDIR_ILIC);
+      while((errNum = WlzNextInterval(&iWsp)) == WLZ_ERR_NONE)
+      {
+	pos.vtY = iWsp.linpos;
+	for(pos.vtX = iWsp.lftpos; pos.vtX <= iWsp.rgtpos; ++pos.vtX)
+	{
+	  WlzCentralityUpdate2D(&fNum, &fDnm, cmV, nRay, pTbl, 1.0, pos);
+	}
+      }
+    }
+    else
+    {
+      errNum = WlzInitGreyScan(fObj, &iWsp, &gWsp);
+      while((errNum = WlzNextGreyInterval(&iWsp)) == WLZ_ERR_NONE)
+      {
+	gPix = gWsp.u_grintptr;
+	pos.vtY = iWsp.linpos;
+	for(pos.vtX = iWsp.lftpos; pos.vtX <= iWsp.rgtpos; ++pos.vtX)
+	{
+	  switch(gWsp.pixeltype)
+	  {
+	    case WLZ_GREY_INT:
+	      WlzCentralityUpdate2D(&fNum, &fDnm, cmV, nRay, pTbl,
+	                            *(gPix.inp)++, pos);
+	      break;
+	    case WLZ_GREY_SHORT:
+	      WlzCentralityUpdate2D(&fNum, &fDnm, cmV, nRay, pTbl,
+	                            *(gPix.shp)++, pos);
+	      break;
+	    case WLZ_GREY_UBYTE:
+	      WlzCentralityUpdate2D(&fNum, &fDnm, cmV, nRay, pTbl,
+	                            *(gPix.ubp)++, pos);
+	      break;
+	    case WLZ_GREY_FLOAT:
+	      WlzCentralityUpdate2D(&fNum, &fDnm, cmV, nRay, pTbl,
+	                            *(gPix.flp)++, pos);
+	      break;
+	    case WLZ_GREY_DOUBLE:
+	      WlzCentralityUpdate2D(&fNum, &fDnm, cmV, nRay, pTbl,
+	                            *(gPix.dbp)++, pos);
+	      break;
+	    default:
+	      errNum = WLZ_ERR_GREY_TYPE;
+	      break;
+	  }
+	}
+      }
+    }
+    if(errNum == WLZ_ERR_EOO)
+    {
+      errNum = WLZ_ERR_NONE;
+    }
+  }
+  if(errNum == WLZ_ERR_NONE)
+  {
+    cent = (fabs(fDnm) > DBL_EPSILON)? fNum / fDnm: DBL_MAX;
+  }
+  AlcFree(pTbl);
+  if(dstErr)
+  {
+    *dstErr = errNum;
+  }
+  return(cent);
+}
+
+/*!
+* \ingroup	WlzFeatures
+* \brief	Computes the polar table with quantized angles as the index
+* 		and maximum radius from the boundary object's centre of mass
+* 		to it's boundary as the value.
+* \param	cmV			Position of the centre of mass.
+* \param	pTblSz			Number of angle increments and
+* 					table size.
+* \param	pTbl			The polar table which is to have it's
+* 					values computed.
+* \param	bndObj			The boundary domain object.
+*/
+static void	WlzCentralityCompPolarTbl2D(WlzDVertex2 cmV, 
+			        int pTblSz, double *pTbl, WlzObject *bndObj)
+{
+  int		idx;
+  double	rad;
+
+  for(idx = 0; idx < pTblSz; ++idx)
+  {
+    pTbl[idx] = -1.0;
+  }
+  WlzCentralityCompPolarTblBnd2D(cmV, pTblSz, pTbl, bndObj->domain.b);
+  /* Fill in any gaps in the table. */
+  idx = 0;
+  rad = -1.0;
+  while(idx < pTblSz)
+  {
+    if(pTbl[idx] > 0.0)
+    {
+      rad = pTbl[idx];
+    }
+    else
+    {
+      pTbl[idx] = rad;
+    }
+    ++idx;
+  }
+  idx = 0;
+  while((pTbl[idx] < 0.0) && (idx < pTblSz))
+  {
+    pTbl[idx] = rad;
+    ++idx;
+  }
+}
+
+/*!
+* \ingroup	WlzFeatures
+* \brief	Computes values of the polar table for a single boundary
+* 		polyline.
+* \param	cmV			Position of the centre of mass.
+* \param	pTblSz			Number of angle increments in the
+* 					polar angle/distance table.
+* \param	pTbl			Polar angle/distance table.
+* \param	ply			Given polyline.
+*/
+static void	WlzCentralityCompPolarTblPly2D(WlzDVertex2 cmV, 
+			        int pTblSz, double *pTbl,
+				WlzPolygonDomain *ply)
+{
+  int		idA,
+  		idV;
+  double	ang,
+  		rad,
+		pTblSz1;
+  WlzVertexP	vtxP;
+  WlzDVertex2	tmpV;
+
+  vtxP.i2 = ply->vtx;
+  pTblSz1 = pTblSz - 0.1;
+  switch(ply->type)
+  {
+    case WLZ_POLYGON_INT:
+      for(idV = 0; idV < ply->nvertices; ++idV)
+      {
+	tmpV.vtX = vtxP.i2[idV].vtX;
+	tmpV.vtY = vtxP.i2[idV].vtY;
+	ang = WlzGeomPolar2D(cmV, tmpV, &rad);
+	idA = (int )(floor((ang * pTblSz1) / (2.0 * ALG_M_PI)));
+	if(rad > pTbl[idA])
+	{
+	  pTbl[idA] = rad;
+	}
+      }
+      break;
+    case WLZ_POLYGON_FLOAT:
+      for(idV = 0; idV < ply->nvertices; ++idV)
+      {
+	tmpV.vtX = vtxP.f2[idV].vtX;
+	tmpV.vtY = vtxP.f2[idV].vtY;
+	ang = WlzGeomPolar2D(cmV, tmpV, &rad);
+	idA = (int )(floor((ang * pTblSz1) / (2.0 * ALG_M_PI)));
+	if(rad > pTbl[idA])
+	{
+	  pTbl[idA] = rad;
+	}
+      }
+      break;
+    case WLZ_POLYGON_DOUBLE:
+      for(idV = 0; idV < ply->nvertices; ++idV)
+      {
+	ang = WlzGeomPolar2D(cmV, vtxP.d2[idV], &rad);
+	idA = (int )(floor((ang * pTblSz1) / (2.0 * ALG_M_PI)));
+	if(rad > pTbl[idA])
+	{
+	  pTbl[idA] = rad;
+	}
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+/*!
+* \ingroup	WlzFeatures
+* \brief	Computes values of the polar table for a boundary
+* 		list. This is a recursive function.
+* \param	cmV			Position of the centre of mass.
+* \param	pTblSz			Number of angle increments in the
+* 					polar angle/distance table.
+* \param	pTbl			Polar angle/distance table.
+* \param	bndLst			Given boundary list.
+*/
+static void	WlzCentralityCompPolarTblBnd2D(WlzDVertex2 cmV, 
+			        int pTblSz, double *pTbl,
+				WlzBoundList  *bndLst)
+{
+  if(bndLst)
+  {
+    WlzCentralityCompPolarTblPly2D(cmV, pTblSz, pTbl, bndLst->poly);
+    WlzCentralityCompPolarTblBnd2D(cmV, pTblSz, pTbl, bndLst->next);
+    WlzCentralityCompPolarTblBnd2D(cmV, pTblSz, pTbl, bndLst->down);
+  }
+}
+
+/*!
+* \ingroup	WlzFeatures
+* \brief	Updates the numererator and denominator sums for the given
+* 		position. See WlzCentrality().
+* \param	fNum			Pointer to current numererator
+* 					for update.
+* \param	fDnm			Pointer to current denominator
+* 					for update.
+* \param	cmV			Position of the centre of mass.
+* \param	pTblSz			Number of angle increments in the
+* 					polar angle/distance table.
+* \param	pTbl			Polar angle/distance table.
+* \param	mass			Mass at given position.
+* \param	pos			Given position.
+*/
+static void	WlzCentralityUpdate2D(double *fNum, double *fDnm,
+				WlzDVertex2 cmV,
+				int pTblSz, double *pTbl,
+				double mass, WlzIVertex2 pos)
+{
+  int		idA;
+  double	ang,
+  		rad,
+		tmp;
+  WlzDVertex2	posD;
+
+  posD.vtX = pos.vtX;
+  posD.vtY = pos.vtY;
+  ang = WlzGeomPolar2D(cmV, posD, &rad);
+  idA = (int )(floor((ang * (pTblSz - 1.0)) / (2.0 * ALG_M_PI)));
+  tmp= mass * (pTbl[idA] - rad);
+  *fNum += tmp * tmp;
+  tmp = mass * pTbl[idA];
+  *fDnm += tmp * tmp;
+}
+
