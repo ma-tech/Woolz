e8c037510434c77a02d497d8a0afde550b622d04
bill <bill>
2009-02-10 13:10:21 +0000

    Tidied up and made provenance clear.

--- libWlzBnd/WlzBndProto.h
+++ libWlzBnd/WlzBndProto.h
@@ -45,56 +45,109 @@ static char _WlzBndProto_h[] = "MRC HGU $Id$";
 */
 
 
-		#ifdef  __cplusplus
-		extern "C" {
-		#endif /* __cplusplus */
+#ifdef  __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-		/************************************************************************
-		* Wlz2DContains.c							*
-		************************************************************************/
-		extern WlzErrorNum		WlzBndGreyInvert(
-						WlzObject *obj);
+/************************************************************************
+* Wlz2DContains.c
+************************************************************************/
+extern WlzErrorNum		WlzBndGreyInvert(
+				  WlzObject *obj);
 
-		/************************************************************************
-		* WlzJavaUtils.c							*
-		************************************************************************/
-		extern int WlzBndObjGetType(WlzObject *obj);
-		extern int WlzBndGetIntervalDomainLine1(WlzObject *obj);
-		extern int WlzBndGetIntervalDomainLastLn(WlzObject *obj);
-		extern int WlzBndGetIntervalDomainKol1(WlzObject *obj);
-		extern int WlzBndGetIntervalDomainLastKl(WlzObject *obj);
-		extern int WlzBndGetPlaneDomainPlane1(WlzObject *obj);
-		extern int WlzBndGetPlaneDomainLastPl(WlzObject *obj);
-		extern int WlzBndGetPlaneDomainLine1(WlzObject *obj);
-		extern int WlzBndGetPlaneDomainLastLn(WlzObject *obj);
-		extern int WlzBndGetPlaneDomainKol1(WlzObject *obj);
-		extern int WlzBndGetPlaneDomainLastKl(WlzObject *obj);
-		extern WlzObject *WlzBndSetGreyValues(WlzObject *domainObj, WlzObject *valuesObj,
-								WlzErrorNum *dstErr);
-		extern WlzAffineTransform *WlzBndGetAffineTransform(
-								WlzThreeDViewStruct *viewStr,
-								WlzErrorNum *dstErr);
-		extern void WlzBndGetTransformMatrix(WlzIVertex2 *dstSizeArrayDat,
-						     double  ***dstArrayDat,
-						     WlzAffineTransform *trans,
-						     WlzErrorNum *dstErr);
+/************************************************************************
+* WlzJavaUtils.c
+************************************************************************/
+extern int 			WlzBndObjGetType(
+				  WlzObject *obj);
+extern int 			WlzBndGetIntervalDomainLine1(
+				  WlzObject *obj);
+extern int 			WlzBndGetIntervalDomainLastLn(
+				  WlzObject *obj);
+extern int 			WlzBndGetIntervalDomainKol1(
+				  WlzObject *obj);
+extern int 			WlzBndGetIntervalDomainLastKl(
+				  WlzObject *obj);
+extern int 			WlzBndGetPlaneDomainPlane1(
+				  WlzObject *obj);
+extern int 			WlzBndGetPlaneDomainLastPl(
+				  WlzObject *obj);
+extern int 			WlzBndGetPlaneDomainLine1(
+				  WlzObject *obj);
+extern int 			WlzBndGetPlaneDomainLastLn(
+				  WlzObject *obj);
+extern int 			WlzBndGetPlaneDomainKol1(
+				  WlzObject *obj);
+extern int 			WlzBndGetPlaneDomainLastKl(
+				  WlzObject *obj);
+extern WlzObject 		*WlzBndSetGreyValues(
+				  WlzObject *domainObj,
+				  WlzObject *valuesObj,
+				  WlzErrorNum *dstErr);
+extern WlzAffineTransform 	*WlzBndGetAffineTransform(
+				  WlzThreeDViewStruct *viewStr,
+				  WlzErrorNum *dstErr);
+extern void 			WlzBndGetTransformMatrix(
+				  WlzIVertex2 *dstSizeArrayDat,
+				  double  ***dstArrayDat,
+				  WlzAffineTransform *trans,
+				  WlzErrorNum *dstErr);
 
-		extern WlzObject *WlzBnd3DWarpObj(WlzObject *wObjS, int arraySizeVec0, WlzDVertex3 *arrayVec0, int arraySizeVec1, WlzDVertex3 *arrayVec1, WlzErrorNum *dstErr);
-		extern WlzObject *WlzBnd3DWarpFile(char *inFileStr, char *TiePointsFileStr, char *outFileStr, WlzErrorNum *dstErr);
-		
-		extern WlzBasisFn *WlzBndBasisFnMQ3DFromCPts(int arraySizeVec0,	WlzDVertex3 *arrayVec0,
-						int arraySizeVec1, WlzDVertex3 *arrayVec1,
-						double delta, WlzErrorNum *dstErr);
-		extern WlzDVertex3	WlzBndBasisFnTransformVertexD3(WlzBasisFnTransform *basisTr,
-							WlzDVertex3 srcVx, WlzErrorNum *dstErr);
-                extern WlzBasisFnTransform *WlzBndBasisFnTrFromCPts3(WlzFnType type,
-					  int order, int arraySizeVec0, WlzDVertex3 *arrayVec0,
-					  int arraySizeVec1, WlzDVertex3 *arrayVec1,
-					  WlzErrorNum *dstErr);
-		extern WlzErrorNum WlzSetVoxelSize(WlzObject *obj, double x, double y, double z);
-		extern WlzObjectType WlzGetObjectType(WlzObject *obj);
-		extern WlzErrorNum WlzExplode(int *dstExpObjCount, WlzObject ***dstExpObjVecP, WlzObject *srcObj);
-		extern WlzObject *WlzGetContourObj(WlzObject *inObj);
-		extern const char *WlzGetPropName(WlzObject *obj);
-		extern int WlzDestroyObj(WlzObject *obj);
+/************************************************************************
+* WlzBnd3DWarp.c
+************************************************************************/
+extern WlzObject 		*WlzBnd3DWarpObj(
+				  WlzObject *wObjS,
+				  int arraySizeVec0,
+				  WlzDVertex3 *arrayVec0,
+				  int arraySizeVec1,
+				  WlzDVertex3 *arrayVec1,
+				  WlzErrorNum *dstErr);
+extern WlzObject 		*WlzBnd3DWarpFile(
+				  char *inFileStr,
+				  char *TiePointsFileStr,
+				  char *outFileStr,
+				  WlzErrorNum *dstErr);
+/************************************************************************
+* WlzBndBasisFn.c
+************************************************************************/
+extern WlzBasisFn 		*WlzBndBasisFnMQ3DFromCPts(
+				  int arraySizeVec0,
+				  WlzDVertex3 *arrayVec0,
+				  int arraySizeVec1,
+				  WlzDVertex3 *arrayVec1,
+				  double delta,
+				  WlzErrorNum *dstErr);
+extern WlzDVertex3		WlzBndBasisFnTransformVertexD3(
+				  WlzBasisFnTransform *basisTr,
+				  WlzDVertex3 srcVx,
+				  WlzErrorNum *dstErr);
+extern WlzBasisFnTransform 	*WlzBndBasisFnTrFromCPts3(
+				  WlzFnType type,
+				  int order,
+				  int arraySizeVec0,
+				  WlzDVertex3 *arrayVec0,
+				  int arraySizeVec1,
+				  WlzDVertex3 *arrayVec1,
+				  WlzErrorNum *dstErr);
+/************************************************************************
+* WlzBndFunction.c
+************************************************************************/
+extern WlzErrorNum 		WlzSetVoxelSize(
+				  WlzObject *obj,
+				  double x,
+				  double y,
+				  double z);
+extern WlzObjectType 		WlzGetObjectType(
+				  WlzObject *obj);
+extern WlzErrorNum 		WlzExplode(
+				  int *dstExpObjCount,
+				  WlzObject ***dstExpObjVecP,
+				  WlzObject *srcObj);
+extern WlzObject 		*WlzGetContourObj(
+				  WlzObject *inObj);
+extern const char 		*WlzGetPropName(
+				  WlzObject *obj);
+extern int 			WlzDestroyObj(
+				  WlzObject *obj);
 #endif	/* !WLZBND_PROTO_H Don't put anything after this line */
