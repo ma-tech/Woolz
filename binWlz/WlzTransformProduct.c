f4100dcda0da66e7880b10b74f5b9d98ec426e1b
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2012-11-27 13:52:04 +0000

    New binary to compute the product of any two transforms.

--- binWlz/WlzTransformProduct.c
+++ binWlz/WlzTransformProduct.c
@@ -68,15 +68,16 @@ Computes the product of a pair of transforms.
 \verbatim
 WlzTransformProduct -o t2.wlz t0.wlz t1.wlz
 \endverbatim
-Computes the product of \f$t_0\f$ and \f$t_1\f$,
-such that \f$t_2\f$ = \f$t_1 t_0\f$, i.e. transform \f$t_0\f$ is applied first
-then \f$t_1\f$.
+Computes the product of \f$\mathbf{T_0}\f$ and \f$\mathbf{T_1}\f$,
+such that \f$\mathbf{T_2}\f$ = \f$\mathbf{T_1} \mathbf{T_0}\f$,
+i.e. transform \f$\mathbf{T_0}\f$ is applied first then \f$\mathbf{T_1}\f$.
 \par File
 \ref WlzTransformProduct.c "WlzTransformProduct.c"
 \par See Also
 \ref BinWlz "WlzIntro(1)"
 \ref wlzaffinetransformobj "WlzAffineTransformObj(1)"
-\ref WlzAffineTransformProduct "WlzAffineTransformProduct(3)"
+\ref wlztransformproduct "WlzTransformProduct(3)"
+\ref wlzaffinetransformproduct "WlzAffineTransformProduct(3)"
 */
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
@@ -95,25 +96,22 @@ extern int      optind,
 
 int             main(int argc, char **argv)
 {
-  int		idx,
-  		option,
+  int		option,
 		ok = 1,
 		usage = 0;
-  WlzAffineTransform *t[3];
-  WlzValues	trVal;
-  WlzDomain	trDom;
-  WlzObject	*obj = NULL;
+  WlzTransform  t[3];
   WlzErrorNum	errNum = WLZ_ERR_NONE;
   FILE		*fP = NULL;
   char 		*outObjFileStr = NULL;
   char		*inObjFileStr[2];
+  const char	*errMsgStr;
   static char	optList[] = "ho:",
 		outFileStrDef[] = "-",
   		inObjFileStrDef[] = "-";
 
   opterr = 0;
-  t[0] = t[1] = t[2] = NULL;
   outObjFileStr = outFileStrDef;
+  t[0].core = t[1].core = t[2].core = NULL;
   inObjFileStr[0] = inObjFileStr[1] = inObjFileStrDef;
   while(ok && ((option = getopt(argc, argv, optList)) != -1))
   {
@@ -151,98 +149,151 @@ int             main(int argc, char **argv)
   }
   if(ok)
   {
-    idx = 0;
-    while(ok && (idx < 2))
+    int		i;
+    WlzObject	*obj = NULL;
+
+    for(i = 0; i < 2; ++i)
     {
-      if((inObjFileStr[idx] == NULL) ||
-	 (*inObjFileStr[idx] == '\0') ||
-	 ((fP = (strcmp(inObjFileStr[idx], "-")?
-		fopen(inObjFileStr[idx], "r"): stdin)) == NULL) ||
+      if((*inObjFileStr[i] == '\0') ||
+	 ((fP = (strcmp(inObjFileStr[i], "-")?
+		fopen(inObjFileStr[i], "r"): stdin)) == NULL) ||
 	 ((obj = WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL) ||
 	 (errNum != WLZ_ERR_NONE))
       {
 	ok = 0;
-	(void )fprintf(stderr,
-		       "%s: Failed to read object from file %s.\n",
-		       *argv, inObjFileStr[idx]);
       }
-      if(fP && strcmp(inObjFileStr[idx], "-"))
+      if(fP && strcmp(inObjFileStr[i], "-"))
       {
-	fclose(fP);
+	(void )fclose(fP);
       }
       if(ok)
       {
-	if((obj->type != WLZ_AFFINE_TRANS) ||
-	   (obj->domain.core == NULL) ||
-	   ((obj->domain.core->type != (WlzObjectType )
-	                               WLZ_TRANSFORM_2D_AFFINE) &&
-	    (obj->domain.core->type != (WlzObjectType )
-	                               WLZ_TRANSFORM_3D_AFFINE)))
+        switch(obj->type)
 	{
-	  ok = 0;
-	  (void )fprintf(stderr,
-			 "%s: Invalid object read from file %s.\n",
-			 *argv, inObjFileStr[idx]);
+	  case WLZ_EMPTY_OBJ:
+	    t[i].empty = WlzMakeEmptyTransform(&errNum);
+	    (void )WlzAssignTransform(t[i], NULL);
+	    break;
+	  case WLZ_TRANS_OBJ:             /* FALLTHROUGH */
+	  case WLZ_AFFINE_TRANS:
+	    t[i].affine = obj->domain.t;
+	    (void )WlzAssignTransform(t[i], NULL);
+	    break;
+	  case WLZ_MESH_TRANS:
+	    t[i].mesh = obj->domain.mt;
+	    (void )WlzAssignTransform(t[i], NULL);
+	    break;
+	  case WLZ_TRANSFORM_2D5_MESH:    /* FALLTHROUGH */
+	  case WLZ_TRANSFORM_3D_MESH:
+	    errNum = WLZ_ERR_UNIMPLEMENTED;
+	    break;
+	  case WLZ_CMESH_2D:              /* FALLTHROUGH */
+	  case WLZ_CMESH_2D5:             /* FALLTHROUGH */
+	  case WLZ_CMESH_3D:
+	    t[i].obj = WlzAssignObject(obj, NULL);
+	    break;
+	  default:
+	    errNum = WLZ_ERR_TRANSFORM_TYPE;
+	    break;
+
 	}
       }
-      if(ok)
+      (void )WlzFreeObj(obj);
+      if(!ok)
       {
-	t[idx] = WlzAssignAffineTransform(obj->domain.t, NULL);
+	(void )WlzStringFromErrorNum(errNum, &errMsgStr);
+	(void )fprintf(stderr,
+		       "%s: Failed to read transform from file %s (%s).\n",
+		       *argv, inObjFileStr[i], errMsgStr);
+        break;
       }
-      if(obj)
-      {
-	(void )WlzFreeObj(obj);
-	obj = NULL;
-      }
-      ++idx;
     }
   }
   if(ok)
   {
-    t[3] = WlzAffineTransformProduct(t[0], t[1], &errNum);
-    if((t[3] == NULL) || (errNum != WLZ_ERR_NONE))
+    t[3] = WlzTransformProduct(t[0], t[1], &errNum);
+    if((t[3].core == NULL) || (errNum != WLZ_ERR_NONE))
     {
       ok = 0;
-      (void )fprintf(stderr, "%s: Failed to compute product.\n", *argv);
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr, "%s: Failed to compute product (%s).\n",
+                     *argv, errMsgStr);
     }
   }
   if(ok)
   {
-    trDom.t = WlzAssignAffineTransform(t[3], NULL);
-    trVal.core = NULL;
-    obj = WlzMakeMain(WLZ_AFFINE_TRANS, trDom, trVal, NULL, NULL, &errNum);
+    WlzObject	*obj = NULL;
+    WlzDomain	dom;
+    WlzValues	val;
+
+    switch(t[3].core->type)
+    {
+      case WLZ_TRANSFORM_EMPTY:
+        obj = WlzAssignObject(
+	      WlzMakeEmpty(&errNum),
+	      NULL);
+        break;
+      case WLZ_TRANSFORM_2D_REG:  	/* FALLTHROUGH */
+      case WLZ_TRANSFORM_2D_TRANS:      /* FALLTHROUGH */
+      case WLZ_TRANSFORM_2D_NOSHEAR:    /* FALLTHROUGH */
+      case WLZ_TRANSFORM_2D_AFFINE:     /* FALLTHROUGH */
+      case WLZ_TRANSFORM_3D_REG:        /* FALLTHROUGH */
+      case WLZ_TRANSFORM_3D_TRANS:      /* FALLTHROUGH */
+      case WLZ_TRANSFORM_3D_NOSHEAR:    /* FALLTHROUGH */
+      case WLZ_TRANSFORM_3D_AFFINE:
+	dom.t = t[3].affine;
+	val.core = NULL;
+	obj = WlzAssignObject(
+	      WlzMakeMain(WLZ_AFFINE_TRANS, dom, val, NULL, NULL, &errNum),
+	      NULL);
+        break;
+      case WLZ_TRANSFORM_2D_MESH:
+	dom.mt = t[3].mesh;
+	val.core = NULL;
+	obj = WlzAssignObject(
+	      WlzMakeMain(WLZ_MESH_TRANS, dom, val, NULL, NULL, &errNum),
+	      NULL);
+        break;
+      case WLZ_TRANSFORM_2D_CMESH:	/* FALLTHROUGH */
+      case WLZ_TRANSFORM_2D5_CMESH:	/* FALLTHROUGH */
+      case WLZ_TRANSFORM_3D_CMESH:
+	obj = WlzAssignObject(t[3].obj, NULL);
+        break;
+      default:
+        errNum = WLZ_ERR_TRANSFORM_TYPE;
+	break;
+    }
     if(errNum != WLZ_ERR_NONE)
     {
       ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
       (void )fprintf(stderr,
-		     "%s: Failed to make transform object.\n",
-		     *argv);
+		     "%s: Failed to make transform object (%s).\n",
+		     *argv, errMsgStr);
     }
     else
     {
-      t[3] = NULL;
       if(((fP = (strcmp(outObjFileStr, "-")?
 		fopen(outObjFileStr, "w"):
 		stdout)) == NULL) ||
 	  (WlzWriteObj(fP, obj) != WLZ_ERR_NONE))
       {
 	ok = 0;
+        (void )WlzStringFromErrorNum(errNum, &errMsgStr);
 	(void )fprintf(stderr,
-		       "%s: failed to write output transform object\n",
-		       *argv);
+		       "%s: Failed to write transform product object (%s).\n",
+		       *argv, errMsgStr);
       }
       if(fP && strcmp(outObjFileStr, "-"))
       {
 	fclose(fP);
       }
       (void )WlzFreeObj(obj);
-      obj = NULL;
     }
   }
-  for(idx = 0; idx < 3; ++idx)
-  {
-    (void )WlzFreeAffineTransform(t[idx]);
-  }
+  (void )WlzFreeTransform(t[0]);
+  (void )WlzFreeTransform(t[1]);
+  (void )WlzFreeTransform(t[2]);
   if(usage)
   {
     (void )fprintf(stderr,
