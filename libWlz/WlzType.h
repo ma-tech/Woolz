8616edc895c0b065f192187a7cac9a288e2efd07
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2014-03-07 10:37:01 +0000

    Simplified region connected calculus masks.

--- libWlz/WlzType.h
+++ libWlz/WlzType.h
@@ -618,44 +618,47 @@ typedef enum _WlzDistanceType
 } WlzDistanceType;
 
 /*!
-* \enum 	_WlzRegConRCC
+* \enum 	_WlzRCCClass
 * \ingroup	WlzType
 * \brief	A Discrete Region Connected Calculus (RCC) clasification
 * 		of an ordered pair of spatial regions. The classifications
 * 		are bit masks to allow for multiple classifications.
 * 		See also WlzRegConCalcRCC().
 */
-typedef enum _WlzRegConRCC
+typedef enum _WlzRCCClass
 {
-  WLZ_REGCON_RCC_EMPTY	= 0, 		/*!< Empty:
+  WLZ_RCC_EMPTY	= 0, 			/*!< Empty:
   					     One or both of the domains is
 					     empty. */
-  WLZ_REGCON_RCC_DC	= 1, 		/*!< Disconnected. */
-  WLZ_REGCON_RCC_EC	= (1<<1),	/*!< Edge Connected. */
-  WLZ_REGCON_RCC_EQ	= (1<<2),	/*!< Equal. */
-  WLZ_REGCON_RCC_PO	= (1<<3),	/*!< Partial Overlap. */
-  WLZ_REGCON_RCC_TPP	= (1<<4),	/*!< Tangential Proper Part. */
-  WLZ_REGCON_RCC_NTPP	= (1<<5),	/*!< Non-Tangential Proper Part. */
-  WLZ_REGCON_RCC_TPPI	= (1<<6),	/*!< Tangential Proper Part inverse. */
-  WLZ_REGCON_RCC_NTPPI	= (1<<7),	/*!< Non-Tangential Proper Part
+  WLZ_RCC_DC	= 1,	 		/*!< Disconnected. */
+  WLZ_RCC_EC	= (1<<1),		/*!< Edge Connected. */
+  WLZ_RCC_EQ	= (1<<2),		/*!< Equal. */
+  WLZ_RCC_PO	= (1<<3),		/*!< Partial Overlap. */
+  WLZ_RCC_TPP	= (1<<4),		/*!< Tangential Proper Part. */
+  WLZ_RCC_NTPP	= (1<<5),		/*!< Non-Tangential Proper Part. */
+  WLZ_RCC_TPPI	= (1<<6),		/*!< Tangential Proper Part inverse. */
+  WLZ_RCC_NTPPI	= (1<<7),		/*!< Non-Tangential Proper Part
   				     	     inverse. */
-  WLZ_REGCON_RCC_SUR	= (1<<8),	/*!< Surrounds:
+  WLZ_RCC_SUR	= (1<<8),		/*!< Surrounds:
   					     The first domain is surrounded by
 					     and edge connected to the
 					     second domain. */
-  WLZ_REGCON_RCC_SURI	= (1<<9),	/*!< Surrounds inverse:
+  WLZ_RCC_SURI	= (1<<9),		/*!< Surrounds inverse:
   					     The second domain is surrounded by
 					     and edge connected to the first
 					     domain.. */
-  WLZ_REGCON_RCC_ENC	= (1<<10),	/*!< Encloses:
+  WLZ_RCC_ENC	= (1<<10),		/*!< Encloses:
   					     The majority of the first domain
 					     is within the convex hull of the
 					     second domain. */
-  WLZ_REGCON_RCC_ENCI	= (1<<11),	/*!< Encloses inverse:
+  WLZ_RCC_ENCI	= (1<<11),		/*!< Encloses inverse:
   					     The majority of the second domain
 					     is within the convex hull of the
 					     first domain. */
-} WlzRegConRCC;
+  WLZ_RCC_MSK    = ((1<<12)-1)   /*!< Not a clasification but a bit
+  					     mask for all the possible
+					     classifications. */
+} WlzRCCClass;
 
 /*!
 * \enum		_WlzSpecialStructElmType
