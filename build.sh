760a199a555c7ee1880123f8296c47bff8e734b2
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2015-05-19 15:50:41 +0100

    Removing JavaWoolz to its own repository.

--- build.sh
+++ build.sh
@@ -22,10 +22,6 @@ export MA=/opt/MouseAtlas
 # This requires jpeg, nifti and tiff libraries.
 ./configure --prefix=$MA --enable-optimise --enable-openmp --enable-extff --with-nifti=$MA --with-jpeg=$MA --with-tiff=$MA --enable-test
 
-# Build optimised, using openmp, external file formats and the Java binding.
-# This requires javacc; the jpeg, nifti and tiff libraries.
-#./configure --prefix=$MA --enable-optimise --enable-openmp --enable-extff --with-jpeg=$MA --with-tiff=$MA --with-nifti=$MA --enable-static --disable-shared --with-pic --enable-java --with-jdk=/opt/java --with-javacc=/opt/JavaCC/bin/javacc
-
 # Build the core Woolz code unoptimised with debug support.
 # This requires only standard system libraries.
 #./configure --prefix=$MA --enable-debug
@@ -41,9 +37,4 @@ export MA=/opt/MouseAtlas
 # Build the core Woolz code optimised with test code.
 # This requires only standard system libraries.
 #./configure --prefix=$MA --enable-optimise --enable-openmp --enable-test
-
-# Build the Woolz code unoptimised with debug support, external file formats,
-# the Java binding and tests.
-# This requires javacc; the jpeg, nifti and tiff libraries.
-#./configure --prefix=$MA --enable-debug --enable-extff --enable-test --with-nifti=$MA --enable-shared --enable-java --with-jdk=/opt/java --with-javacc=/opt/JavaCC/bin/javacc
 
