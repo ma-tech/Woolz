81a1442708ef2580d6d5dc85894ed41b0005e05f
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2017-03-27 11:47:08 +0100

    Added binWlz/WlzDitherPoints.c.

---
+++ binWlz/WlzDitherPoints.c
@@ -0,0 +1,256 @@
+#if defined(__GNUC__)
+#ident "University of Edinburgh $Id$"
+#else
+static char _WlzDitherPoints_c[] = "University of Edinburgh $Id$";
+#endif
+/*!
+* \file         binWlz/WlzDitherPoints.c
+* \author       Bill Hill
+* \date         April 2003
+* \version      $Id$
+* \par
+* Address:
+*               MRC Human Genetics Unit,
+*               MRC Institute of Genetics and Molecular Medicine,
+*               University of Edinburgh,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par
+* Copyright (C), [2012],
+* The University Court of the University of Edinburgh,
+* Old College, Edinburgh, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \brief	Creates a domain with a marker located at the position
+* 		of each point vertex using a points object.
+* \ingroup	BinWlz
+*
+* \par Binary
+* \ref wlzpointstomarkers "WlzDitherPoints"
+*/
+
+/*!
+\ingroup BinWlz
+\defgroup wlzpointstomarkers  WlzDitherPoints
+\par Name
+WlzDitherPoints - dithers point positions.
+\par Synopsis
+\verbatim
+WlzDitherPoints [-h] [-o<output file>] [-D #,#[#]] [<input file>]
+\endverbatim
+\par Options
+<table width="500" border="0">
+  <tr> 
+    <td><b>-h</b></td>
+    <td>Help, prints usage message.</td>
+  </tr>
+  <tr> 
+    <td><b>-o</b></td>
+    <td>Output file name, default standard output.</td>
+  </tr>
+  <tr> 
+    <td><b>-D</b></td>
+    <td>Dither the points by applying a random offset. Supplied values
+        are the maximum dither displacement. Default value is (1,1,1).</td>
+  </tr>
+</table>
+\par Description
+Reads a 2D or 3D points object from either a given file or the
+standard input (default). The vertices of the points are then
+converted to floating point, dithered and output.
+\par Examples
+\verbatim
+WlzDitherPoints -o out.wlz -D 1,1,1 in.wlz
+\endverbatim
+Creates a new points file where the point vertices are double precision and
+dithered by up to a voxel in each direction.
+The inpup points are read from in.wlz and the output points written to out.wlz.
+\par File
+\ref binWlz/WlzDitherPoints.c "WlzDitherPoints.c"
+\par See Also
+\ref BinWlz "WlzIntro(1)"
+\ref WlzDitherPoints "WlzDitherPoints.c(1)"
+\ref WlzPointsDither "WlzPointsDither(3)"
+*/
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <float.h>
+#include <sys/time.h>
+#include <Wlz.h>
+
+extern int      getopt(int argc, char * const *argv, const char *optstring);
+ 
+extern char     *optarg;
+extern int 	optind,
+		opterr,
+		optopt;
+
+int		main(int argc, char *argv[])
+{
+  int		ok = 1,
+  		option,
+		usage = 0;
+  char		*inFileStr,
+		*outFileStr;
+  FILE		*fP = NULL;
+  WlzDVertex3	ditherSz = {1.0, 1.0, 1.0};
+  WlzObject	*inObj = NULL,
+		*outObj = NULL;
+  WlzErrorNum	errNum = WLZ_ERR_NONE;
+  const char	*errMsgStr;
+  static char	optList[] = "ho:D:t:";
+  const char    fileStrDef[] = "-";
+
+  /* Parse the argument list and check for input files. */
+  opterr = 0;
+  inFileStr = (char *)fileStrDef;
+  outFileStr = (char *)fileStrDef;
+  while(ok && !usage && ((option = getopt(argc, argv, optList)) != EOF))
+  {
+    switch(option)
+    {
+      case 'D':
+	if(sscanf(optarg, "%lg,%lg,%lg",
+	          &(ditherSz.vtX), &(ditherSz.vtY), &(ditherSz.vtZ)) < 2)
+	{
+	  usage = 1;
+	}
+	break;
+      case 'o':
+	outFileStr = optarg;
+	break;
+      case 'h':
+      default:
+	usage = 1;
+	break;
+    }
+  }
+  if(ok)
+  {
+    ok = !usage;
+  }
+  if(ok && (optind < argc))
+  {
+    if((optind + 1) != argc)
+    {
+      usage = 1;
+      ok = 0;
+    }
+    else
+    {
+      inFileStr = argv[optind];
+    }
+  }
+  /* Read the input object. */
+  if(ok)
+  {
+    if((inFileStr == NULL) ||
+       (*inFileStr == '\0') ||
+       ((fP = (strcmp(inFileStr, "-")?
+              fopen(inFileStr, "r"): stdin)) == NULL) ||
+       ((inObj = WlzAssignObject(WlzReadObj(fP, &errNum), NULL)) == NULL))
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: Failed to read input object from file %s.\n",
+                     argv[0], inFileStr);
+    }
+    if(fP && strcmp(inFileStr, "-"))
+    {
+      (void )fclose(fP); fP = NULL;
+    }
+  }
+  if(ok)
+  {
+    WlzDomain dDom;
+
+    dDom.pts = WlzPointsDither(inObj->domain.pts, ditherSz, &errNum);
+    if(errNum == WLZ_ERR_NONE)
+    {
+      outObj = WlzMakeMain(inObj->type, dDom, inObj->values, inObj->plist,
+      			   NULL, &errNum);
+    }
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+                     "%s: Failed to dither points %s.\n",
+		     argv[0], errMsgStr);
+    }
+  }
+  /* Output the points object. */
+  if(ok)
+  {
+    if((fP = (strcmp(outFileStr, "-")?
+             fopen(outFileStr, "w"): stdout)) == NULL)
+    {
+      ok = 0;
+      (void )fprintf(stderr,
+                     "%s: Failed to open output file %s.\n",
+                     argv[0], outFileStr);
+    }
+  }
+  if(ok)
+  {
+    errNum = WlzWriteObj(fP, outObj);
+    if(errNum != WLZ_ERR_NONE)
+    {
+      ok = 0;
+      (void )WlzStringFromErrorNum(errNum, &errMsgStr);
+      (void )fprintf(stderr,
+                     "%s: Failed to write output object, %s.\n",
+                     argv[0], errMsgStr);
+    }
+  }
+  if(fP && strcmp(outFileStr, "-"))
+  {
+    (void )fclose(fP); fP = NULL;
+  }
+  (void )WlzFreeObj(inObj);
+  (void )WlzFreeObj(outObj);
+  if(usage)
+  {
+    (void )fprintf(stderr,
+    "Usage: %s [-D#,#[,#]] [-o <output file>] [-h] [<input file>]\n"
+    "Dithers point positions. A 2D or 3D points object is read either from\n"
+    "a given file or the standard input (default). The vertices of the\n"
+    "points are then converted to floating point, dithered and output\n"
+    "either to the given file or the standard output (default).\n"
+    "Version: %s\n"
+    "Options:\n"
+    "  -D  Dither the points by applying a random offset. Supplied values\n"
+    "      are the maximum dither displacement. Default value (1, 1, 1).\n"
+    "  -o  Output object file.\n"
+    "  -h  Help - prints this usage message\n"
+    "By default all files are read from the standard input and written to\n"
+    "the standard output.\n"
+    "Example:\n"
+    "  %s -o out.wlz -D 1,1,1 in.wlz\n"
+    "The input points are read from in.wlz and points dithered using a\n"
+    "maximum displacement of 1,1,1 are written to out.wlz.\n",
+    argv[0],
+    WlzVersion(),
+    argv[0]);
+  }
+  return(!ok);
+}
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
